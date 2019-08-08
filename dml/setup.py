fe3cf8fa4b73560f06a01bc35d5cb1f14ee13b64
malte <malte@scalable.capital>
2019-08-08 11:25:04 +0200

    initialised python package

---
+++ dml/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dml",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="Double Machine Learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
