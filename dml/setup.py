c0dcd4bb4eca3539891ccfe7104d6430b0de7f1f
malte <malte@scalable.capital>
2019-08-08 15:41:36 +0200

    minor corrections in setup file

--- dml/setup.py
+++ dml/setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="dml",
     version="0.0.1",
     author="Example Author",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
