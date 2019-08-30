2ca52cea3c571e75794d741cc1dfaf6157e16f1b
malte <malte@scalable.capital>
2019-08-30 16:36:49 +0200

    add iv dgp

--- dml/dml/tests/test_plr.py
+++ dml/dml/tests/test_plr.py
@@ -10,7 +10,7 @@ from sklearn.base import clone
 from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.ensemble import RandomForestRegressor
 
-from dml.double_ml_plr import DoubleMLPLR
+from ..double_ml_plr import DoubleMLPLR
 
 def g(x):
     return np.power(np.sin(x),2)
@@ -44,6 +44,35 @@ def generate_data1(request):
         D = M+np.random.standard_normal(size=[N,])
         Y = np.dot(theta,D)+G+np.random.standard_normal(size=[N,])
         xx = {'X': X, 'y': Y, 'd': D}
+        datasets.append(xx)
+    
+    return datasets
+    
+@pytest.fixture(scope="module",
+                params = [(1000, 20)])
+def generate_data_iv(request):
+    N_p = request.param
+    np.random.seed(1111)
+    # setting parameters
+    N = N_p[0]
+    p = N_p[1]
+    theta=0.5
+    gamma_z=0.4
+    b= [1/k for k in range(1,p+1)]
+    sigma = make_spd_matrix(p)
+    
+    # generating data
+    datasets = []
+    for i in range(n_datasets):
+        X = np.random.multivariate_normal(np.ones(p),sigma,size=[N,])
+        G = g(np.dot(X,b))
+        # instrument 
+        Z = m(np.dot(X,b)) + np.random.standard_normal(size=[N,])
+        M = m(gamma_z * Z + np.dot(X,b))
+        # treatment
+        D = M + np.random.standard_normal(size=[N,])
+        Y = np.dot(theta,D)+G+np.random.standard_normal(size=[N,])
+        xx = {'X': X, 'y': Y, 'd': D, 'z': Z}
         datasets.append(xx)
     
     return datasets
