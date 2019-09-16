5a995e7111125ccf904e2cc2889c0ad44fbe71cc
malte <malte@scalable.capital>
2019-09-16 12:11:27 +0200

    started implementing unit tests for irm; not finished yet; still many failing tests

---
+++ dml/dml/tests/helper_irm_manual.py
@@ -0,0 +1,136 @@
+import numpy as np
+import pytest
+import math
+import scipy
+
+
+def fit_nuisance_irm(Y, X, D, ml_m, ml_g, smpls):
+    g_hat0 = []
+    g_hat1 = []
+    for idx, (train_index, test_index) in enumerate(smpls):
+        train_index0 =np.intersect1d(np.where(D==0)[0], train_index)
+        g_hat0.append(ml_g.fit(X[train_index0],Y[train_index0]).predict(X[test_index]))
+    
+    for idx, (train_index, test_index) in enumerate(smpls):
+        train_index0 =np.intersect1d(np.where(D==0)[0], train_index)
+        train_index1 =np.setdiff1d(train_index, train_index0)
+        g_hat1.append(ml_g.fit(X[train_index1],Y[train_index1]).predict(X[test_index]))
+    
+    m_hat = []
+    for idx, (train_index, test_index) in enumerate(smpls):
+        m_hat.append(ml_m.fit(X[train_index],D[train_index]).predict_proba(X[test_index])[:, 1])
+    
+    return g_hat0, g_hat1, m_hat
+
+def irm_dml1(Y, X, D, g_hat0, g_hat1, m_hat, smpls, inf_model):
+    thetas = np.zeros(len(smpls))
+    
+    for idx, (train_index, test_index) in enumerate(smpls):
+        v_hat = D[test_index] - m_hat[idx]
+        u_hat0 = Y[test_index] - g_hat0[idx]
+        u_hat1 = Y[test_index] - g_hat1[idx]
+        thetas[idx] = irm_orth(g_hat0[idx], g_hat1[idx],
+                               m_hat[idx], u_hat0, u_hat1,
+                               D[test_index], inf_model)
+    theta_hat = np.mean(thetas)
+    
+    ses = np.zeros(len(smpls))
+    for idx, (train_index, test_index) in enumerate(smpls):
+        u_hat0 = Y[test_index] - g_hat0[idx]
+        u_hat1 = Y[test_index] - g_hat1[idx]
+        ses[idx] = var_irm(theta_hat, g_hat0[idx], g_hat1[idx],
+                           m_hat[idx], u_hat0, u_hat1,
+                           D[test_index], inf_model)
+    se = np.sqrt(np.mean(ses))
+    
+    return theta_hat, se
+
+def irm_dml2(Y, X, D, g_hat0, g_hat1, m_hat, smpls, inf_model):
+    thetas = np.zeros(len(smpls))
+    u_hat0 = np.zeros_like(Y)
+    u_hat1 = np.zeros_like(Y)
+    v_hat = np.zeros_like(D)
+    g_hat0_all = np.zeros_like(Y)
+    g_hat1_all = np.zeros_like(Y)
+    m_hat_all = np.zeros_like(D)
+    for idx, (train_index, test_index) in enumerate(smpls):
+        u_hat0[test_index] = Y[test_index] - g_hat0[idx]
+        u_hat1[test_index] = Y[test_index] - g_hat1[idx]
+        g_hat0_all[test_index] = g_hat0[idx]
+        g_hat1_all[test_index] = g_hat1[idx]
+        m_hat_all[test_index] = m_hat[idx]
+    theta_hat = irm_orth(g_hat0_all, g_hat1_all, m_hat_all, u_hat0, u_hat0, D, inf_model)
+    se = np.sqrt(var_irm(theta_hat, g_hat0_all, g_hat1_all,
+                         m_hat_all, u_hat0, u_hat1,
+                         D, inf_model))
+    
+    return theta_hat, se
+    
+def var_irm(theta, g_hat0, g_hat1, m_hat, u_hat0, u_hat1, D, inf_model):
+    n_obs = len(D)
+    
+    #if se_type == 'ATE':
+    #    var = 1/n_obs * 1/np.power(np.mean(np.multiply(v_hat, v_hat)), 2) * \
+    #          np.mean(np.power(np.multiply(u_hat - v_hat*theta, v_hat), 2))
+    #elif se_type == 'ATTE':
+    #    var = 1/n_obs * 1/np.power(np.mean(np.multiply(v_hat, d)), 2) * \
+    #          np.mean(np.power(np.multiply(u_hat - d*theta, v_hat), 2))
+    #else:
+    #    raise ValueError('invalid se_type')
+    var = 1.0
+    
+    return var
+
+def irm_orth(g_hat0, g_hat1, m_hat, u_hat0, u_hat1, D, inf_model):
+    if inf_model == 'ATE':
+        res = np.mean(g_hat1 - g_hat0 \
+                      + np.divide(np.multiply(D, u_hat1), m_hat) \
+                      + np.divide(np.multiply(1.-D, u_hat0), 1.-m_hat))
+    elif inf_model == 'ATTE':
+        Ep = np.mean(D)
+        
+        res = np.mean(np.multiply(D, u_hat0)/Ep \
+                      - np.divide(np.multiply(m_hat, np.multiply(1.-D, u_hat0)), Ep*(1.-m_hat))) \
+              / np.mean(D/Ep)
+    
+    return res
+
+def boot_irm(theta, Y, D, g_hat, m_hat, smpls, inf_model, se, bootstrap, n_rep):
+    u_hat = np.zeros_like(Y)
+    v_hat = np.zeros_like(D)
+    for idx, (train_index, test_index) in enumerate(smpls):
+        v_hat[test_index] = D[test_index] - m_hat[idx]
+        u_hat[test_index] = Y[test_index] - g_hat[idx]
+    
+    if inf_model == 'DML2018':
+        score = np.multiply(u_hat - v_hat*theta, v_hat)
+        J = np.mean(-np.multiply(v_hat, v_hat))
+    elif inf_model == 'IV-type':
+        score = np.multiply(u_hat - D*theta, v_hat)
+        J = np.mean(-np.multiply(v_hat, D))
+    else:
+        raise ValueError('invalid se_type')
+    
+    n_obs = len(score)
+    boot_theta = np.zeros(n_rep)
+    if bootstrap == 'wild':
+        # if method wild for unit test comparability draw all rv at one step
+        xx_sample = np.random.normal(loc=0.0, scale=1.0, size=(n_rep, n_obs))
+        yy_sample = np.random.normal(loc=0.0, scale=1.0, size=(n_rep, n_obs))
+    
+    for i_rep in range(n_rep):
+        if bootstrap == 'Bayes':
+            weights = np.random.exponential(scale=1.0, size=n_obs) - 1.
+        elif bootstrap == 'normal':
+            weights = np.random.normal(loc=0.0, scale=1.0, size=n_obs)
+        elif bootstrap == 'wild':
+            xx = xx_sample[i_rep,:]
+            yy = yy_sample[i_rep,:]
+            weights = xx / np.sqrt(2) + (np.power(yy,2) - 1)/2
+        else:
+            raise ValueError('invalid bootstrap method')
+        
+        boot_theta[i_rep] = np.mean(np.multiply(np.divide(weights, se),
+                                               score / J))
+    
+    return boot_theta
---
+++ dml/dml/tests/test_irm.py
@@ -0,0 +1,76 @@
+import numpy as np
+import pytest
+import math
+import scipy
+
+from sklearn.model_selection import KFold
+from sklearn.base import clone
+
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+
+from dml.double_ml_irm import DoubleMLPIRM
+
+from dml.tests.helper_general import get_n_datasets
+from dml.tests.helper_irm_manual import irm_dml1, irm_dml2, fit_nuisance_irm, boot_irm
+
+
+# number of datasets per dgp
+n_datasets = get_n_datasets()
+
+
+@pytest.mark.parametrize('idx', range(n_datasets))
+@pytest.mark.parametrize('learner', [[LogisticRegression(solver='lbfgs'),
+                                      LinearRegression()],
+                                     [RandomForestClassifier(max_depth=2, n_estimators=10),
+                                      RandomForestRegressor(max_depth=2, n_estimators=10)]])
+@pytest.mark.parametrize('inf_model', ['ATE', 'ATTE'])
+@pytest.mark.parametrize('dml_procedure', ['dml1', 'dml2'])
+def test_dml_irm(generate_data_irm, idx, learner, inf_model, dml_procedure):
+    resampling = KFold(n_splits=2, shuffle=True)
+    
+    # Set machine learning methods for m & g
+    ml_learners = {'ml_m': clone(clone(learner[0])),
+                   'ml_g': clone(clone(learner[1]))}
+    
+    dml_irm_obj = DoubleMLPIRM(resampling,
+                               ml_learners,
+                               dml_procedure,
+                               inf_model)
+    data = generate_data_irm[idx]
+    np.random.seed(3141)
+    dml_irm_obj.fit(data['X'], data['y'], data['d'])
+    
+    np.random.seed(3141)
+    smpls = [(train, test) for train, test in resampling.split(data['X'])]
+    
+    g_hat0, g_hat1, m_hat = fit_nuisance_irm(data['y'], data['X'], data['d'],
+                                             clone(learner[0]), clone(learner[1]), smpls)
+    
+    if dml_procedure == 'dml1':
+        res_manual, se_manual = irm_dml1(data['y'], data['X'], data['d'],
+                                         g_hat0, g_hat1, m_hat,
+                                         smpls, inf_model)
+    elif dml_procedure == 'dml2':
+        res_manual, se_manual = irm_dml2(data['y'], data['X'], data['d'],
+                                         g_hat0, g_hat1, m_hat,
+                                         smpls, inf_model)
+    
+    assert math.isclose(dml_irm_obj.coef_, res_manual, rel_tol=1e-9, abs_tol=1e-4)
+    #assert math.isclose(dml_irm_obj.se_[0], se_manual, rel_tol=1e-9, abs_tol=1e-4)
+    #
+    #for bootstrap in ['normal']:
+    #    np.random.seed(3141)
+    #    boot_theta = boot_irm(res_manual,
+    #                          data['y'], data['d'],
+    #                          g_hat, m_hat,
+    #                          smpls, inf_model,
+    #                          se_manual,
+    #                          bootstrap, 500)
+    #    
+    #    np.random.seed(3141)
+    #    dml_irm_obj.bootstrap(method = bootstrap, n_rep=500)
+    #    assert np.allclose(dml_irm_obj.boot_coef_, boot_theta, rtol=1e-9, atol=1e-4)
+    
+    return
+    
