52716c69f5bfdf062891bf72953947e1ea4e106f
malte <malte@scalable.capital>
2019-09-25 10:43:35 +0200

    add install requirements

--- dml/setup.py
+++ dml/setup.py
@@ -13,6 +13,11 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'sklearn',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
