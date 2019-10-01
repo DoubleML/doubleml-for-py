bug fix: Wrong scaling factor for dml1 standard error

dml/dml/double_ml.py
@@ -262,9 +262,9 @@ class DoubleML(ABC):
             score = score[inds]
         
         # don't understand yet the additional 1/n_obs
-        n_obs_sample = len(score)
+        n_obs = self.n_obs
         J = np.mean(score_a)
-        sigma2_hat = 1/n_obs_sample * np.mean(np.power(score, 2)) / np.power(J, 2)
+        sigma2_hat = 1/n_obs * np.mean(np.power(score, 2)) / np.power(J, 2)
         
         return sigma2_hat    
         

dml/dml/tests/helper_iivm_manual.py
@@ -34,6 +34,7 @@ def fit_nuisance_iivm(Y, X, D, Z, ml_m, ml_g, ml_r, smpls):
 
 def iivm_dml1(Y, X, D, Z, g_hat0, g_hat1, m_hat, r_hat0, r_hat1, smpls, inf_model):
     thetas = np.zeros(len(smpls))
+    n_obs = len(Y)
     
     for idx, (train_index, test_index) in enumerate(smpls):
         u_hat0 = Y[test_index] - g_hat0[idx]
@@ -59,12 +60,13 @@ def iivm_dml1(Y, X, D, Z, g_hat0, g_hat1, m_hat, r_hat0, r_hat1, smpls, inf_mode
                             r_hat0[idx], r_hat1[idx],
                             u_hat0, u_hat1,
                             w_hat0, w_hat1,
-                            Z[test_index], inf_model)
+                            Z[test_index], inf_model, n_obs)
     se = np.sqrt(np.mean(ses))
     
     return theta_hat, se
 
 def iivm_dml2(Y, X, D, Z, g_hat0, g_hat1, m_hat, r_hat0, r_hat1, smpls, inf_model):
+    n_obs = len(Y)
     u_hat0 = np.zeros_like(Y)
     u_hat1 = np.zeros_like(Y)
     w_hat0 = np.zeros_like(Y)
@@ -89,13 +91,11 @@ def iivm_dml2(Y, X, D, Z, g_hat0, g_hat1, m_hat, r_hat0, r_hat1, smpls, inf_mode
     se = np.sqrt(var_iivm(theta_hat, g_hat0_all, g_hat1_all,
                           m_hat_all, r_hat0_all, r_hat1_all,
                           u_hat0, u_hat1, w_hat0, w_hat1,
-                          Z, inf_model))
+                          Z, inf_model, n_obs))
     
     return theta_hat, se
     
-def var_iivm(theta, g_hat0, g_hat1, m_hat, r_hat0, r_hat1, u_hat0, u_hat1, w_hat0, w_hat1, Z, se_type):
-    n_obs = len(Z)
-    
+def var_iivm(theta, g_hat0, g_hat1, m_hat, r_hat0, r_hat1, u_hat0, u_hat1, w_hat0, w_hat1, Z, se_type, n_obs):
     if se_type == 'LATE':
         var = 1/n_obs * np.mean(np.power(g_hat1 - g_hat0 \
                             + np.divide(np.multiply(Z, u_hat1), m_hat) \

dml/dml/tests/helper_irm_manual.py
@@ -30,6 +30,7 @@ def fit_nuisance_irm(Y, X, D, ml_m, ml_g, smpls, inf_model):
 
 def irm_dml1(Y, X, D, g_hat0, g_hat1, m_hat, p_hat, smpls, inf_model):
     thetas = np.zeros(len(smpls))
+    n_obs = len(Y)
     
     for idx, (train_index, test_index) in enumerate(smpls):
         u_hat0 = Y[test_index] - g_hat0[idx]
@@ -47,12 +48,13 @@ def irm_dml1(Y, X, D, g_hat0, g_hat1, m_hat, p_hat, smpls, inf_model):
         ses[idx] = var_irm(theta_hat, g_hat0[idx], g_hat1[idx],
                            m_hat[idx], p_hat[idx],
                            u_hat0, u_hat1,
-                           D[test_index], inf_model)
+                           D[test_index], inf_model, n_obs)
     se = np.sqrt(np.mean(ses))
     
     return theta_hat, se
 
 def irm_dml2(Y, X, D, g_hat0, g_hat1, m_hat, p_hat, smpls, inf_model):
+    n_obs = len(Y)
     u_hat0 = np.zeros_like(Y)
     u_hat1 = np.zeros_like(Y)
     g_hat0_all = np.zeros_like(Y)
@@ -71,13 +73,11 @@ def irm_dml2(Y, X, D, g_hat0, g_hat1, m_hat, p_hat, smpls, inf_model):
     se = np.sqrt(var_irm(theta_hat, g_hat0_all, g_hat1_all,
                          m_hat_all, p_hat_all,
                          u_hat0, u_hat1,
-                         D, inf_model))
+                         D, inf_model, n_obs))
     
     return theta_hat, se
     
-def var_irm(theta, g_hat0, g_hat1, m_hat, p_hat, u_hat0, u_hat1, D, se_type):
-    n_obs = len(D)
-    
+def var_irm(theta, g_hat0, g_hat1, m_hat, p_hat, u_hat0, u_hat1, D, se_type, n_obs):
     if se_type == 'ATE':
         var = 1/n_obs * np.mean(np.power(g_hat1 - g_hat0 \
                       + np.divide(np.multiply(D, u_hat1), m_hat) \

dml/dml/tests/helper_pliv_manual.py
@@ -21,6 +21,7 @@ def fit_nuisance_pliv(Y, X, D, Z, ml_m, ml_g, ml_r, smpls):
 
 def pliv_dml1(Y, X, D, Z, g_hat, m_hat, r_hat, smpls, inf_model):
     thetas = np.zeros(len(smpls))
+    n_obs = len(Y)
     
     for idx, (train_index, test_index) in enumerate(smpls):
         u_hat = Y[test_index] - g_hat[idx]
@@ -36,13 +37,14 @@ def pliv_dml1(Y, X, D, Z, g_hat, m_hat, r_hat, smpls, inf_model):
         w_hat = D[test_index] - r_hat[idx]
         ses[idx] = var_pliv(theta_hat, D[test_index],
                             u_hat, v_hat, w_hat,
-                            inf_model)
+                            inf_model, n_obs)
     se = np.sqrt(np.mean(ses))
     
     return theta_hat, se
 
 def pliv_dml2(Y, X, D, Z, g_hat, m_hat, r_hat, smpls, inf_model):
     thetas = np.zeros(len(smpls))
+    n_obs = len(Y)
     u_hat = np.zeros_like(Y)
     v_hat = np.zeros_like(Z)
     w_hat = np.zeros_like(D)
@@ -51,13 +53,11 @@ def pliv_dml2(Y, X, D, Z, g_hat, m_hat, r_hat, smpls, inf_model):
         v_hat[test_index] = Z[test_index] - m_hat[idx]
         w_hat[test_index] = D[test_index] - r_hat[idx]
     theta_hat = pliv_orth(u_hat, v_hat, w_hat, D, inf_model)
-    se = np.sqrt(var_pliv(theta_hat, D, u_hat, v_hat, w_hat, inf_model))
+    se = np.sqrt(var_pliv(theta_hat, D, u_hat, v_hat, w_hat, inf_model, n_obs))
     
     return theta_hat, se
     
-def var_pliv(theta, d, u_hat, v_hat, w_hat, se_type):
-    n_obs = len(u_hat)
-    
+def var_pliv(theta, d, u_hat, v_hat, w_hat, se_type, n_obs):
     if se_type == 'DML2018':
         var = 1/n_obs * 1/np.power(np.mean(np.multiply(v_hat, w_hat)), 2) * \
               np.mean(np.power(np.multiply(u_hat - w_hat*theta, v_hat), 2))

dml/dml/tests/helper_plr_manual.py
@@ -17,6 +17,7 @@ def fit_nuisance_plr(Y, X, D, ml_m, ml_g, smpls):
 
 def plr_dml1(Y, X, D, g_hat, m_hat, smpls, inf_model):
     thetas = np.zeros(len(smpls))
+    n_obs = len(Y)
     
     for idx, (train_index, test_index) in enumerate(smpls):
         v_hat = D[test_index] - m_hat[idx]
@@ -30,26 +31,25 @@ def plr_dml1(Y, X, D, g_hat, m_hat, smpls, inf_model):
         u_hat = Y[test_index] - g_hat[idx]
         ses[idx] = var_plr(theta_hat, D[test_index],
                            u_hat, v_hat,
-                           inf_model)
+                           inf_model, n_obs)
     se = np.sqrt(np.mean(ses))
     
     return theta_hat, se
 
 def plr_dml2(Y, X, D, g_hat, m_hat, smpls, inf_model):
     thetas = np.zeros(len(smpls))
+    n_obs = len(Y)
     u_hat = np.zeros_like(Y)
     v_hat = np.zeros_like(D)
     for idx, (train_index, test_index) in enumerate(smpls):
         v_hat[test_index] = D[test_index] - m_hat[idx]
         u_hat[test_index] = Y[test_index] - g_hat[idx]
     theta_hat = plr_orth(v_hat, u_hat, D, inf_model)
-    se = np.sqrt(var_plr(theta_hat, D, u_hat, v_hat, inf_model))
+    se = np.sqrt(var_plr(theta_hat, D, u_hat, v_hat, inf_model, n_obs))
     
     return theta_hat, se
     
-def var_plr(theta, d, u_hat, v_hat, se_type):
-    n_obs = len(u_hat)
-    
+def var_plr(theta, d, u_hat, v_hat, se_type, n_obs):
     if se_type == 'DML2018':
         var = 1/n_obs * 1/np.power(np.mean(np.multiply(v_hat, v_hat)), 2) * \
               np.mean(np.power(np.multiply(u_hat - v_hat*theta, v_hat), 2))