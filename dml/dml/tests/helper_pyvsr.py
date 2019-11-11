ca04cd1e1bd27d55d35bbf2bcaf8973f786b5dba
malte <malte@scalable.capital>
2019-11-11 17:54:09 +0100

    add unit tests to cross-check the python vs the R implementation

---
+++ dml/dml/tests/helper_pyvsr.py
@@ -0,0 +1,28 @@
+from rpy2.robjects import pandas2ri
+from rpy2 import robjects
+pandas2ri.activate()
+
+# The R code to fit the DML model
+r_dml = robjects.r('''
+        library('dml')
+        library('mlr3learners')
+        library('data.table')
+        library('mlr3')
+
+        f <- function(data, inf_model, dml_procedure) {
+            data = data.table(data)
+            mlmethod <- list(mlmethod_m = 'regr.lm',
+                     mlmethod_g = 'regr.lm')
+            params <- list(params_g = list(),
+                           params_m = list())
+
+            double_mlplr_obj = DoubleMLPLR$new(n_folds = 2,
+                                     ml_learners = mlmethod,
+                                     params = params,
+                                     dml_procedure = dml_procedure, inf_model = inf_model)
+            double_mlplr_obj$fit(data, y = "y", d = "d")
+            return(list(coef = double_mlplr_obj$coef,
+                        se = double_mlplr_obj$se))
+        }
+        ''')
+
---
+++ dml/dml/tests/test_plr_pyvsr.py
@@ -0,0 +1,81 @@
+import pytest
+import math
+
+from sklearn.model_selection import KFold
+from sklearn.base import clone
+
+from sklearn.linear_model import LinearRegression
+
+from dml.double_ml_data import DoubleMLData
+from dml.double_ml_plr import DoubleMLPLR
+
+from dml.tests.helper_general import get_n_datasets
+
+from rpy2.robjects import pandas2ri
+from dml.tests.helper_pyvsr import r_dml
+
+# number of datasets per dgp
+n_datasets = get_n_datasets()
+
+@pytest.fixture(scope='module',
+                params = range(n_datasets))
+def idx(request):
+    return request.param
+
+@pytest.fixture(scope='module',
+                params = ['IV-type', 'DML2018'])
+def inf_model(request):
+    return request.param
+
+
+@pytest.fixture(scope='module',
+                params = ['dml1', 'dml2'])
+def dml_procedure(request):
+    return request.param
+
+
+
+@pytest.fixture(scope="module")
+def dml_plr_fixture(generate_data1, idx, inf_model, dml_procedure):
+
+    resampling = KFold(n_splits=2, shuffle=False)
+    
+    # Set machine learning methods for m & g
+    learner = LinearRegression()
+    ml_learners = {'ml_m': clone(clone(learner)),
+                   'ml_g': clone(clone(learner))}
+    
+    dml_plr_obj = DoubleMLPLR(resampling,
+                              ml_learners,
+                              dml_procedure,
+                              inf_model)
+    data = generate_data1[idx]
+    #np.random.seed(3141)
+    X_cols = data.columns[data.columns.str.startswith('X')].tolist()
+    obj_dml_data = DoubleMLData(data, X_cols, 'y', ['d'])
+    dml_plr_obj.fit(obj_dml_data)
+
+    # fit the DML model in R
+    r_dataframe = pandas2ri.py2rpy(data)
+    res_r = r_dml(r_dataframe, inf_model, dml_procedure)
+
+    
+    res_dict = {'coef_py': dml_plr_obj.coef,
+                'coef_r': res_r[0],
+                'se_py': dml_plr_obj.se,
+                'se_r': res_r[1]}
+
+    
+    return res_dict
+
+
+def test_dml_plr_pyvsr_coef(dml_plr_fixture):
+    assert math.isclose(dml_plr_fixture['coef_py'],
+                        dml_plr_fixture['coef_r'],
+                        rel_tol=1e-9, abs_tol=1e-4)
+
+
+def test_dml_plr_pyvsr_se(dml_plr_fixture):
+    assert math.isclose(dml_plr_fixture['se_py'],
+                        dml_plr_fixture['se_r'],
+                        rel_tol=1e-9, abs_tol=1e-4)
