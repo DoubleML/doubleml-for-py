ce9bd9ee6723592c64738d7bb6f7a42dad0719f6
Malte S. Kurz <malte.s.kurz@gmail.com>
2020-09-15 08:30:34 +0200

    temporarily switch themes to get sphinx-tabs working

--- doc/conf.py
+++ doc/conf.py
@@ -64,7 +64,7 @@ autosummary_generate = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'sphinx_book_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
