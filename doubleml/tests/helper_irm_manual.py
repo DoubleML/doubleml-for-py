659538e7e264122c96788e2dfbd9967d1a103866
Malte S. Kurz <malte.s.kurz@gmail.com>
2020-09-18 14:47:46 +0200

    refactoring of manual bootstrap computations

--- doubleml/tests/helper_irm_manual.py
+++ doubleml/tests/helper_irm_manual.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from doubleml.tests.helper_boot import boot_manual
 
 def fit_nuisance_irm(Y, X, D, ml_m, ml_g, smpls, score):
     g_hat0 = []
@@ -142,34 +143,7 @@ def boot_irm(theta, Y, D, g_hat0, g_hat1, m_hat, p_hat, smpls, score, se, bootst
                 - theta * np.divide(D, p_hat_all)
     else:
         raise ValueError('invalid score')
-    
-    n_obs = len(psi)
-    boot_theta = np.zeros(n_rep)
-    if bootstrap == 'wild':
-        # if method wild for unit test comparability draw all rv at one step
-        xx_sample = np.random.normal(loc=0.0, scale=1.0, size=(n_rep, n_obs))
-        yy_sample = np.random.normal(loc=0.0, scale=1.0, size=(n_rep, n_obs))
-    
-    for i_rep in range(n_rep):
-        if bootstrap == 'Bayes':
-            weights = np.random.exponential(scale=1.0, size=n_obs) - 1.
-        elif bootstrap == 'normal':
-            weights = np.random.normal(loc=0.0, scale=1.0, size=n_obs)
-        elif bootstrap == 'wild':
-            xx = xx_sample[i_rep,:]
-            yy = yy_sample[i_rep,:]
-            weights = xx / np.sqrt(2) + (np.power(yy,2) - 1)/2
-        else:
-            raise ValueError('invalid bootstrap method')
 
-        if dml_procedure == 'dml1':
-            this_boot_theta = np.zeros(n_folds)
-            for idx, (train_index, test_index) in enumerate(smpls):
-                this_boot_theta[idx] = np.mean(np.multiply(np.divide(weights[test_index], se),
-                                                           psi[test_index] / J[idx]))
-            boot_theta[i_rep] = np.mean(this_boot_theta)
-        elif dml_procedure == 'dml2':
-            boot_theta[i_rep] = np.mean(np.multiply(np.divide(weights, se),
-                                                    psi / J))
+    boot_theta = boot_manual(psi, J, smpls, se, bootstrap, n_rep, dml_procedure)
     
     return boot_theta
