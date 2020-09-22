bcc6ba2800082956abb668c3564321e661a77920
Malte S. Kurz <malte.s.kurz@gmail.com>
2020-09-22 11:27:09 +0200

    unit test for IRM tuning

--- doubleml/tests/helper_irm_manual.py
+++ doubleml/tests/helper_irm_manual.py
@@ -1,29 +1,75 @@
 import numpy as np
+from sklearn.model_selection import KFold, GridSearchCV
+from sklearn.base import clone
 
 
-def fit_nuisance_irm(Y, X, D, ml_m, ml_g, smpls, score):
+def fit_nuisance_irm(Y, X, D, learner_m, learner_g, smpls, score, g0_params=None, g1_params=None, m_params=None):
+    ml_g0 = clone(learner_g)
+    ml_g1 = clone(learner_g)
     g_hat0 = []
     g_hat1 = []
     for idx, (train_index, test_index) in enumerate(smpls):
-        train_index0 =np.intersect1d(np.where(D==0)[0], train_index)
-        g_hat0.append(ml_g.fit(X[train_index0],Y[train_index0]).predict(X[test_index]))
+        if g0_params is not None:
+            ml_g0.set_params(**g0_params[idx])
+        train_index0 = np.intersect1d(np.where(D == 0)[0], train_index)
+        g_hat0.append(ml_g0.fit(X[train_index0], Y[train_index0]).predict(X[test_index]))
     
     if score == 'ATE':
         for idx, (train_index, test_index) in enumerate(smpls):
-            train_index1 =np.intersect1d(np.where(D==1)[0], train_index)
-            g_hat1.append(ml_g.fit(X[train_index1],Y[train_index1]).predict(X[test_index]))
+            if g1_params is not None:
+                ml_g1.set_params(**g1_params[idx])
+            train_index1 = np.intersect1d(np.where(D == 1)[0], train_index)
+            g_hat1.append(ml_g1.fit(X[train_index1], Y[train_index1]).predict(X[test_index]))
     else:
         for idx, (train_index, test_index) in enumerate(smpls):
             # fill it up, but its not further used
             g_hat1.append(np.zeros_like(g_hat0[idx]))
-    
+
+    ml_m = clone(learner_m)
     m_hat = []
     p_hat = []
     for idx, (train_index, test_index) in enumerate(smpls):
-        m_hat.append(ml_m.fit(X[train_index],D[train_index]).predict_proba(X[test_index])[:, 1])
+        if m_params is not None:
+            ml_m.set_params(**m_params[idx])
+        m_hat.append(ml_m.fit(X[train_index], D[train_index]).predict_proba(X[test_index])[:, 1])
         p_hat.append(np.mean(D[test_index]))
     
     return g_hat0, g_hat1, m_hat, p_hat
+
+
+def tune_nuisance_irm(Y, X, D, ml_m, ml_g, smpls, score, n_folds_tune, param_grid_g, param_grid_m):
+    g0_tune_res = [None] * len(smpls)
+    g1_tune_res = [None] * len(smpls)
+    m_tune_res = [None] * len(smpls)
+
+    for idx, (train_index, test_index) in enumerate(smpls):
+        # cv for ml_g0
+        g0_tune_resampling = KFold(n_splits=n_folds_tune)
+        g0_grid_search = GridSearchCV(ml_g, param_grid_g,
+                                      cv=g0_tune_resampling)
+        train_index0 = np.intersect1d(np.where(D == 0)[0], train_index)
+        g0_tune_res[idx] = g0_grid_search.fit(X[train_index0, :], Y[train_index0])
+
+        if score == 'ATE':
+            # cv for ml_g1
+            g1_tune_resampling = KFold(n_splits=n_folds_tune)
+            g1_grid_search = GridSearchCV(ml_g, param_grid_g,
+                                          cv=g1_tune_resampling)
+            train_index1 = np.intersect1d(np.where(D == 1)[0], train_index)
+            g1_tune_res[idx] = g1_grid_search.fit(X[train_index1, :], Y[train_index1])
+
+        # cv for ml_m
+        m_tune_resampling = KFold(n_splits=n_folds_tune)
+        m_grid_search = GridSearchCV(ml_m, param_grid_m,
+                                     cv=m_tune_resampling)
+        m_tune_res[idx] = m_grid_search.fit(X[train_index, :], D[train_index])
+
+    g0_best_params = [xx.best_params_ for xx in g0_tune_res]
+    g1_best_params = [xx.best_params_ for xx in g1_tune_res]
+    m_best_params = [xx.best_params_ for xx in m_tune_res]
+
+    return g0_best_params, g1_best_params, m_best_params
+
 
 def irm_dml1(Y, X, D, g_hat0, g_hat1, m_hat, p_hat, smpls, score):
     thetas = np.zeros(len(smpls))
@@ -49,6 +95,7 @@ def irm_dml1(Y, X, D, g_hat0, g_hat1, m_hat, p_hat, smpls, score):
     se = np.sqrt(np.mean(ses))
     
     return theta_hat, se
+
 
 def irm_dml2(Y, X, D, g_hat0, g_hat1, m_hat, p_hat, smpls, score):
     n_obs = len(Y)
