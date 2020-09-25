321dd07fcb304da75e502c06c2eede4c36f6a206
Malte S. Kurz <malte.s.kurz@gmail.com>
2020-09-25 16:56:51 +0200

    added py vs r unit tests for the IV models

--- doubleml/tests/test_pliv_pyvsr.py
+++ doubleml/tests/test_pliv_pyvsr.py
@@ -8,7 +8,8 @@ from sklearn.linear_model import LinearRegression
 import doubleml as dml
 
 from doubleml.tests.helper_general import get_n_datasets
-from doubleml.tests.helper_pyvsr import export_smpl_split_to_r, r_MLPLIV
+from doubleml.tests.helper_pyvsr import export_smpl_split_to_r, r_MLPLIV, \
+    r_MLPLIV_PARTIAL_X, r_MLPLIV_PARTIAL_Z, r_MLPLIV_PARTIAL_XZ
 
 from rpy2.robjects import pandas2ri
 pandas2ri.activate()
@@ -86,3 +87,157 @@ def test_dml_pliv_pyvsr_se(dml_pliv_pyvsr_fixture):
                         dml_pliv_pyvsr_fixture['se_r'],
                         rel_tol=1e-9, abs_tol=1e-4)
 
+
+@pytest.fixture(scope='module')
+def dml_pliv_partial_x_pyvsr_fixture(generate_data_pliv_partialX, idx, score, dml_procedure):
+    n_folds = 2
+
+    # collect data
+    data = generate_data_pliv_partialX[idx]
+    X_cols = data.columns[data.columns.str.startswith('X')].tolist()
+    Z_cols = data.columns[data.columns.str.startswith('Z')].tolist()
+
+    # Set machine learning methods for g, m & r
+    learner = LinearRegression()
+    ml_g = clone(learner)
+    ml_m = clone(learner)
+    ml_r = clone(learner)
+
+    np.random.seed(3141)
+    obj_dml_data = dml.DoubleMLData(data, 'y', ['d'], X_cols, Z_cols)
+    dml_pliv_obj = dml.DoubleMLPLIV(obj_dml_data,
+                                    ml_g, ml_m, ml_r,
+                                    n_folds,
+                                    dml_procedure=dml_procedure)
+
+    dml_pliv_obj.fit()
+
+
+    # fit the DML model in R
+    all_train, all_test = export_smpl_split_to_r(dml_pliv_obj.smpls[0])
+
+    r_dataframe = pandas2ri.py2rpy(data)
+    res_r = r_MLPLIV_PARTIAL_X(r_dataframe, 'partialling out', dml_procedure,
+                              all_train, all_test)
+    print(res_r)
+
+    res_dict = {'coef_py': dml_pliv_obj.coef,
+                'coef_r': res_r[0],
+                'se_py': dml_pliv_obj.se,
+                'se_r': res_r[1]}
+
+    return res_dict
+
+
+def test_dml_pliv_partial_x_pyvsr_coef(dml_pliv_partial_x_pyvsr_fixture):
+    assert math.isclose(dml_pliv_partial_x_pyvsr_fixture['coef_py'],
+                        dml_pliv_partial_x_pyvsr_fixture['coef_r'],
+                        rel_tol=1e-9, abs_tol=1e-4)
+
+
+def test_dml_pliv_partial_x_pyvsr_se(dml_pliv_partial_x_pyvsr_fixture):
+    assert math.isclose(dml_pliv_partial_x_pyvsr_fixture['se_py'],
+                        dml_pliv_partial_x_pyvsr_fixture['se_r'],
+                        rel_tol=1e-9, abs_tol=1e-4)
+
+
+@pytest.fixture(scope='module')
+def dml_pliv_partial_z_pyvsr_fixture(generate_data_pliv_partialZ, idx, score, dml_procedure):
+    n_folds = 2
+
+    # collect data
+    data = generate_data_pliv_partialZ[idx]
+    X_cols = data.columns[data.columns.str.startswith('X')].tolist()
+    Z_cols = data.columns[data.columns.str.startswith('Z')].tolist()
+
+    # Set machine learning methods for g, m & r
+    learner = LinearRegression()
+    ml_r = clone(learner)
+
+    np.random.seed(3141)
+    obj_dml_data = dml.DoubleMLData(data, 'y', ['d'], X_cols, Z_cols)
+    dml_pliv_obj = dml.DoubleMLPLIV.partialZ(obj_dml_data,
+                                             ml_r,
+                                             n_folds,
+                                             dml_procedure=dml_procedure)
+
+    dml_pliv_obj.fit()
+
+    # fit the DML model in R
+    all_train, all_test = export_smpl_split_to_r(dml_pliv_obj.smpls[0])
+
+    r_dataframe = pandas2ri.py2rpy(data)
+    res_r = r_MLPLIV_PARTIAL_Z(r_dataframe, 'partialling out', dml_procedure,
+                     all_train, all_test)
+    print(res_r)
+
+    res_dict = {'coef_py': dml_pliv_obj.coef,
+                'coef_r': res_r[0],
+                'se_py': dml_pliv_obj.se,
+                'se_r': res_r[1]}
+
+    return res_dict
+
+
+def test_dml_pliv_partial_z_pyvsr_coef(dml_pliv_partial_z_pyvsr_fixture):
+    assert math.isclose(dml_pliv_partial_z_pyvsr_fixture['coef_py'],
+                        dml_pliv_partial_z_pyvsr_fixture['coef_r'],
+                        rel_tol=1e-9, abs_tol=1e-4)
+
+
+def test_dml_pliv_partial_z_pyvsr_se(dml_pliv_partial_z_pyvsr_fixture):
+    assert math.isclose(dml_pliv_partial_z_pyvsr_fixture['se_py'],
+                        dml_pliv_partial_z_pyvsr_fixture['se_r'],
+                        rel_tol=1e-9, abs_tol=1e-4)
+
+
+@pytest.fixture(scope='module')
+def dml_pliv_partial_xz_pyvsr_fixture(generate_data_pliv_partialXZ, idx, score, dml_procedure):
+    n_folds = 2
+
+    # collect data
+    data = generate_data_pliv_partialXZ[idx]
+    X_cols = data.columns[data.columns.str.startswith('X')].tolist()
+    Z_cols = data.columns[data.columns.str.startswith('Z')].tolist()
+
+    # Set machine learning methods for g, m & r
+    learner = LinearRegression()
+    ml_g = clone(learner)
+    ml_m = clone(learner)
+    ml_r = clone(learner)
+
+    np.random.seed(3141)
+    obj_dml_data = dml.DoubleMLData(data, 'y', ['d'], X_cols, Z_cols)
+    dml_pliv_obj = dml.DoubleMLPLIV.partialXZ(obj_dml_data,
+                                              ml_g, ml_m, ml_r,
+                                              n_folds,
+                                              dml_procedure=dml_procedure)
+
+    dml_pliv_obj.fit()
+
+    # fit the DML model in R
+    all_train, all_test = export_smpl_split_to_r(dml_pliv_obj.smpls[0])
+
+    r_dataframe = pandas2ri.py2rpy(data)
+    res_r = r_MLPLIV_PARTIAL_XZ(r_dataframe, 'partialling out', dml_procedure,
+                     all_train, all_test)
+    print(res_r)
+
+    res_dict = {'coef_py': dml_pliv_obj.coef,
+                'coef_r': res_r[0],
+                'se_py': dml_pliv_obj.se,
+                'se_r': res_r[1]}
+
+    return res_dict
+
+
+def test_dml_pliv_partial_xz_pyvsr_coef(dml_pliv_partial_xz_pyvsr_fixture):
+    assert math.isclose(dml_pliv_partial_xz_pyvsr_fixture['coef_py'],
+                        dml_pliv_partial_xz_pyvsr_fixture['coef_r'],
+                        rel_tol=1e-9, abs_tol=1e-4)
+
+
+def test_dml_pliv_partial_xz_pyvsr_se(dml_pliv_partial_xz_pyvsr_fixture):
+    assert math.isclose(dml_pliv_partial_xz_pyvsr_fixture['se_py'],
+                        dml_pliv_partial_xz_pyvsr_fixture['se_r'],
+                        rel_tol=1e-9, abs_tol=1e-4)
