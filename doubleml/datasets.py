a29a3987582cc34c8dc713eafdc25277b964e6d5
Malte S. Kurz <malte.s.kurz@gmail.com>
2020-10-13 11:57:57 +0200

    add documenation for make_pliv_CHS2015()

--- doubleml/datasets.py
+++ doubleml/datasets.py
@@ -125,7 +125,7 @@ def make_plr_turrell2018(n_obs=100, dim_x=20, theta=0.5, return_type='DoubleMLDa
 
     with covariates :math:`X \\sim \\mathcal{N}(0, \\Sigma)`, where  :math:`\\Sigma` is a random symmetric,
     positive-definite matrix generated with :py:meth:`sklearn.datasets.make_spd_matrix`.
-    :math:`b` is a vector with entries :math:`b_j=\\frac{1}{j} and the nuisance functions are given by
+    :math:`b` is a vector with entries :math:`b_j=\\frac{1}{j}` and the nuisance functions are given by
 
     .. math::
 
@@ -264,6 +264,47 @@ def make_iivm_data(n_obs=100, dim_x=20, theta=0.5, gamma_z=0.4, return_type='Dou
 
 
 def make_pliv_CHS2015(n_obs, alpha=1., dim_x=200, dim_z=150, return_type='DoubleMLData'):
+    """
+    Generates data from a partially linear IV regression model used in Chernozhukov, Hansen and Spindler (2015).
+    The data generating process is defined as
+
+    .. math::
+
+        Z &= \\Pi X + \\zeta,
+
+        D &= X' \\gamma + Z' \\delta + u,
+
+        Y &= \\alpha D + X' \\beta + \\varepsilon,
+
+    with
+
+    .. math::
+
+        \\left(\\begin{matrix} \\varepsilon \\\\ u \\\\ \\zeta \\\\ x \\end{matrix} \\right) \\sim \\mathcal{N}\\left(0, \\left(\\begin{matrix} 1 & 0.6 & 0 & 0 \\\\ 0.6 & 1 & 0 & 0 \\\\ 0 & 0 & 0.25 I_{p_n^z} & 0 \\\\ 0 & 0 & 0 & \\Sigma \\end{matrix} \\right) \\right)
+
+    where  :math:`\\Sigma` is a :math:`p_n^x \\times p_n^x` matrix with entries
+    :math:`\\Sigma_{kj} = 0.5^{|j-k|}` and :math:`I_{p_n^z}` is the :math:`p_n^z \\times p_n^z` identity matrix.
+    :math:`\\beta = \\gamma` is a :math:`p_n^x`-vector with entries :math:`\\beta_j=\\frac{1}{j^2}`,
+    :math:`\\delta` is a :math:`p_n^z`-vector with entries :math:`\\delta_j=\\frac{1}{j^2}`
+    and :math:`\\Pi = (I_{p_n^z}, 0_{p_n^z \\times (p_n^x - p_n^z)})`.
+
+    Parameters
+    ----------
+    n_obs :
+        The number of observations to simulate.
+    alpha :
+        The value of the causal parameter.
+    dim_x :
+        The number of covariates.
+    dim_z :
+        The number of instruments.
+    return_type :
+        .. include:: ../../shared/dgp/return_type.rst
+
+    References
+    ----------
+    Chernozhukov, V., Hansen, C. and Spindler, M. (2015), Post-Selection and Post-Regularization Inference in Linear Models with Many Controls and Instruments. American Economic Review: Papers and Proceedings, 105 (5): 486-90.
+    """
     assert dim_x >= dim_z
     # see https://assets.aeaweb.org/asset-server/articles-attachments/aer/app/10505/P2015_1022_app.pdf
     xx = np.random.multivariate_normal(np.zeros(2),
