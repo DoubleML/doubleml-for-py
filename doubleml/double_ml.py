fix asymptotic variance aggregation for repeated cross-fitting

doubleml/double_ml.py
@@ -848,8 +848,19 @@ class DoubleML(ABC):
         # aggregate parameters from the repeated cross-fitting
         # don't use the getter (always for one treatment variable and one sample), but the private variable
         self.coef = np.median(self._all_coef, 1)
+
+        # TODO: In the documentation of standard errors we need to cleary state what we return here, i.e.,
+        # the asymptotic variance sigma_hat/N and not sigma_hat (which sometimes is also called the asympt var)!
+        if self.apply_cross_fitting:
+            n_obs = self._dml_data.n_obs
+        else:
+            # be prepared for the case of test sets of different size in repeated no-cross-fitting
+            smpls = self.__smpls
+            test_index = smpls[0][1]
+            n_obs = len(test_index)
         xx = np.tile(self.coef.reshape(-1, 1), self.n_rep)
-        self.se = np.sqrt(np.median(np.power(self._all_se, 2) + np.power(self._all_coef - xx, 2), 1))
+        self.se = np.sqrt(np.divide(np.median(np.multiply(np.power(self._all_se, 2), n_obs) +
+                                              np.power(self._all_coef - xx, 2), 1), n_obs))
 
     def _compute_bootstrap(self, method, n_rep):
         dml_procedure = self.dml_procedure

doubleml/tests/test_plr_no_cross_fit.py
@@ -204,7 +204,7 @@ def dml_plr_rep_no_cross_fit_fixture(generate_data1, idx, learner, score, n_rep)
         ses[i_rep] = ses[i_rep]*np.sqrt(len(y)/len(smpls[0][1]))
 
     res_manual = np.median(thetas)
-    se_manual = np.sqrt(np.median(np.power(ses, 2) + np.power(thetas - res_manual, 2)))
+    se_manual = np.sqrt(np.median(np.power(ses, 2)*len(smpls[0][1]) + np.power(thetas - res_manual, 2))/len(smpls[0][1]))
 
     res_dict = {'coef': dml_plr_obj.coef,
                 'coef_manual': res_manual,

doubleml/tests/test_plr_rep_cross.py
@@ -77,6 +77,7 @@ def dml_plr_fixture(generate_data1, idx, learner, score, dml_procedure, n_rep):
     y = data['y'].values
     X = data.loc[:, X_cols].values
     d = data['d'].values
+    n_obs = len(y)
     all_smpls = []
     for i_rep in range(n_rep):
         resampling = KFold(n_splits=n_folds,
@@ -107,7 +108,7 @@ def dml_plr_fixture(generate_data1, idx, learner, score, dml_procedure, n_rep):
                                                  smpls, score)
 
     res_manual = np.median(thetas)
-    se_manual = np.sqrt(np.median(np.power(ses, 2) + np.power(thetas - res_manual, 2)))
+    se_manual = np.sqrt(np.median(np.power(ses, 2)*n_obs + np.power(thetas - res_manual, 2))/n_obs)
     
     res_dict = {'coef': dml_plr_obj.coef,
                 'coef_manual': res_manual,