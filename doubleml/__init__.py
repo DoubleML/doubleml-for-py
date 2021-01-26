c7fa568b45be33975af151408a7fdc50a9ce3f4e
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-01-26 09:33:49 +0100

    define an __all__ for doubleml

--- doubleml/__init__.py
+++ doubleml/__init__.py
@@ -6,4 +6,10 @@ from .double_ml_irm import DoubleMLIRM
 from .double_ml_iivm import DoubleMLIIVM
 from .double_ml_data import DoubleMLData
 
+__all__ = ['DoubleMLPLR',
+           'DoubleMLPLIV',
+           'DoubleMLIRM',
+           'DoubleMLIIVM',
+           'DoubleMLData']
+
 __version__ = get_distribution('doubleml').version
