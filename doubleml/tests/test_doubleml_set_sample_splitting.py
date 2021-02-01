aa8018d9f75868be6f2eeff7dc629112df457e39
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-02-01 17:31:37 +0100

    check in unit tests that drawn sample spitts can be re-entered via set_sample_splitting

--- doubleml/tests/test_doubleml_set_sample_splitting.py
+++ doubleml/tests/test_doubleml_set_sample_splitting.py
@@ -1,18 +1,38 @@
 import pytest
 import numpy as np
 
-from doubleml import DoubleMLPLR, DoubleMLIRM
-from doubleml.datasets import make_plr_CCDDHNR2018, make_irm_data
+from doubleml import DoubleMLPLR
+from doubleml.datasets import make_plr_CCDDHNR2018
 
-from sklearn.linear_model import Lasso, LogisticRegression
-from sklearn.base import BaseEstimator
+from sklearn.linear_model import Lasso
 
 dml_data = make_plr_CCDDHNR2018(n_obs=10)
 ml_g = Lasso()
 ml_m = Lasso()
 dml_plr = DoubleMLPLR(dml_data, ml_g, ml_m,
-                      n_folds=13, n_rep=13,
+                      n_folds=7, n_rep=8,
                       draw_sample_splitting=False)
+
+
+def _assert_resampling_pars(dml_obj0, dml_obj1):
+    assert dml_obj0.n_folds == dml_obj1.n_folds
+    assert dml_obj0.n_rep == dml_obj1.n_rep
+    assert dml_obj0.apply_cross_fitting == dml_obj1.apply_cross_fitting
+    _assert_smpls_equal(dml_obj0.smpls, dml_obj1.smpls, dml_obj0.apply_cross_fitting)
+
+
+def _assert_smpls_equal(smpls0, smpls1, apply_cross_fitting=True):
+    assert len(smpls0) == len(smpls1)
+    for i_rep in range(len(smpls0)):
+        assert len(smpls0[i_rep]) == len(smpls1[i_rep])
+        if apply_cross_fitting:
+            for i_fold in range(len(smpls0[i_rep])):
+                assert np.array_equal(smpls0[i_rep][i_fold][0], smpls1[i_rep][i_fold][0])
+                assert np.array_equal(smpls0[i_rep][i_fold][1], smpls1[i_rep][i_fold][1])
+        else:
+            assert len(smpls0[i_rep]) == 1
+            assert np.array_equal(smpls0[i_rep][0][0], smpls1[i_rep][0][0])
+            assert np.array_equal(smpls0[i_rep][0][1], smpls1[i_rep][0][1])
 
 
 @pytest.mark.ci
@@ -21,10 +41,10 @@ def test_doubleml_set_sample_splitting_tuple():
     smpls = ([0, 1, 2, 3, 4], [5, 6, 7, 8, 9])
     dml_plr.set_sample_splitting(smpls)
 
-    assert np.array_equal(dml_plr.smpls[0][0], smpls)
     assert dml_plr.n_folds == 2
     assert dml_plr.n_rep == 1
     assert not dml_plr.apply_cross_fitting
+    _assert_smpls_equal([[smpls]], dml_plr.smpls, apply_cross_fitting=dml_plr.apply_cross_fitting)
 
     # no cross-fitting, no sample-splitting
     smpls = ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -32,6 +52,7 @@ def test_doubleml_set_sample_splitting_tuple():
     assert dml_plr.n_folds == 1
     assert dml_plr.n_rep == 1
     assert not dml_plr.apply_cross_fitting
+    _assert_smpls_equal([[smpls]], dml_plr.smpls, apply_cross_fitting=dml_plr.apply_cross_fitting)
 
     smpls = ([0, 1, 2, 3, 4], [5, 6], [7, 8, 9])
     msg = 'Invalid partition provided. ' + 'Tuple for train_ind and test_ind must consist of exactly two elements.'
@@ -48,6 +69,7 @@ def test_doubleml_set_sample_splitting_all_tuple():
     assert dml_plr.n_folds == 2
     assert dml_plr.n_rep == 1
     assert dml_plr.apply_cross_fitting
+    _assert_smpls_equal([smpls], dml_plr.smpls, apply_cross_fitting=dml_plr.apply_cross_fitting)
 
     smpls = [([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]),
              ([5, 6, 7, 8, 9], [0, 1, 2], [3, 4])]
@@ -59,10 +81,10 @@ def test_doubleml_set_sample_splitting_all_tuple():
     smpls = [([0, 1, 2, 3, 4], [5, 6, 7, 8, 9])]
     dml_plr.set_sample_splitting(smpls)
 
-    assert np.array_equal(dml_plr.smpls[0], smpls)
     assert dml_plr.n_folds == 2
     assert dml_plr.n_rep == 1
     assert not dml_plr.apply_cross_fitting
+    _assert_smpls_equal([smpls], dml_plr.smpls, apply_cross_fitting=dml_plr.apply_cross_fitting)
 
     # sample splitting with cross-fitting and two folds that do not form a partition
     smpls = [([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]),
@@ -83,6 +105,7 @@ def test_doubleml_set_sample_splitting_all_list():
     assert dml_plr.n_folds == 2
     assert dml_plr.n_rep == 2
     assert dml_plr.apply_cross_fitting
+    _assert_smpls_equal(smpls, dml_plr.smpls, apply_cross_fitting=dml_plr.apply_cross_fitting)
 
     smpls = np.array(([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]))
     # msg = ("all_smpls must be of list or tuple type. [[0 1 2 3 4]"
@@ -142,4 +165,52 @@ def test_doubleml_set_sample_splitting_all_list():
     assert dml_plr.n_folds == 2
     assert dml_plr.n_rep == 3
     assert not dml_plr.apply_cross_fitting
+    _assert_smpls_equal(smpls, dml_plr.smpls, apply_cross_fitting=dml_plr.apply_cross_fitting)
 
+
+@pytest.mark.ci
+def test_doubleml_draw_vs_set():
+    dml_plr_set = DoubleMLPLR(dml_data, ml_g, ml_m, n_folds=7, n_rep=8)
+
+    dml_plr_drawn = DoubleMLPLR(dml_data, ml_g, ml_m,
+                                n_folds=1, n_rep=1, apply_cross_fitting=False)
+    # ToDO: Following cases are not implemented yet
+    # dml_plr_set.set_sample_splitting(dml_plr_drawn.smpls)
+    # _assert_resampling_pars(dml_plr_drawn, dml_plr_set)
+    # dml_plr_set.set_sample_splitting(dml_plr_drawn.smpls[0])
+    # _assert_resampling_pars(dml_plr_drawn, dml_plr_set)
+    dml_plr_set.set_sample_splitting(dml_plr_drawn.smpls[0][0])
+    _assert_resampling_pars(dml_plr_drawn, dml_plr_set)
+
+    dml_plr_drawn = DoubleMLPLR(dml_data, ml_g, ml_m,
+                                n_folds=2, n_rep=1, apply_cross_fitting=False)
+    dml_plr_set.set_sample_splitting(dml_plr_drawn.smpls)
+    _assert_resampling_pars(dml_plr_drawn, dml_plr_set)
+    dml_plr_set.set_sample_splitting(dml_plr_drawn.smpls[0])
+    _assert_resampling_pars(dml_plr_drawn, dml_plr_set)
+    dml_plr_set.set_sample_splitting(dml_plr_drawn.smpls[0][0])
+    _assert_resampling_pars(dml_plr_drawn, dml_plr_set)
+
+    dml_plr_drawn = DoubleMLPLR(dml_data, ml_g, ml_m,
+                                n_folds=2, n_rep=1, apply_cross_fitting=True)
+    dml_plr_set.set_sample_splitting(dml_plr_drawn.smpls)
+    _assert_resampling_pars(dml_plr_drawn, dml_plr_set)
+    dml_plr_set.set_sample_splitting(dml_plr_drawn.smpls[0])
+    _assert_resampling_pars(dml_plr_drawn, dml_plr_set)
+
+    dml_plr_drawn = DoubleMLPLR(dml_data, ml_g, ml_m,
+                                n_folds=5, n_rep=1, apply_cross_fitting=True)
+    dml_plr_set.set_sample_splitting(dml_plr_drawn.smpls)
+    _assert_resampling_pars(dml_plr_drawn, dml_plr_set)
+    dml_plr_set.set_sample_splitting(dml_plr_drawn.smpls[0])
+    _assert_resampling_pars(dml_plr_drawn, dml_plr_set)
+
+    dml_plr_drawn = DoubleMLPLR(dml_data, ml_g, ml_m,
+                                n_folds=5, n_rep=3, apply_cross_fitting=True)
+    dml_plr_set.set_sample_splitting(dml_plr_drawn.smpls)
+    _assert_resampling_pars(dml_plr_drawn, dml_plr_set)
+
+    dml_plr_drawn = DoubleMLPLR(dml_data, ml_g, ml_m,
+                                n_folds=2, n_rep=4, apply_cross_fitting=False)
+    dml_plr_set.set_sample_splitting(dml_plr_drawn.smpls)
+    _assert_resampling_pars(dml_plr_drawn, dml_plr_set)
