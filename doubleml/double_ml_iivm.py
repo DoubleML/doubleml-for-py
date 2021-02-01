DoubleML/doubleml-for-py

commit 8cbb86900c9d39a1783fd62e3f25096403ed635a

make the data compatibility checks more meaningful

doubleml/double_ml_iivm.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.utils import check_X_y
+from sklearn.utils.multiclass import type_of_target
 from sklearn.base import clone
 from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
@@ -134,9 +135,22 @@ class DoubleMLIIVM(DoubleML):
         return score
 
     def _check_data(self, obj_dml_data):
-        assert obj_dml_data.n_treat == 1
-        check_binary_vector(obj_dml_data.d, variable_name='d')
-        check_binary_vector(obj_dml_data.z, variable_name='z')
+        one_treat = (obj_dml_data.n_treat == 1)
+        binary_treat = (type_of_target(obj_dml_data.d) == 'binary')
+        zero_one_treat = np.any((np.power(obj_dml_data.d, 2) - obj_dml_data.d) == 0)
+        if not(one_treat & binary_treat & zero_one_treat):
+            raise ValueError('Incompatible data.\n'
+                             'To fit an IIVM model with DML '
+                             'exactly one binary variable with values 0 and 1 '
+                             'needs to be specified as treatment variable.')
+        one_instr = (obj_dml_data.n_instr == 1)
+        binary_instr = (type_of_target(obj_dml_data.z) == 'binary')
+        zero_one_instr = np.any((np.power(obj_dml_data.z, 2) - obj_dml_data.z) == 0)
+        if not(one_instr & binary_instr & zero_one_instr):
+            raise ValueError('Incompatible data.\n'
+                             'To fit an IIVM model with DML '
+                             'exactly one binary variable with values 0 and 1 '
+                             'needs to be specified as instrumental variable.')
         return
     
     def _get_cond_smpls(self, smpls, z):

doubleml/double_ml_irm.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.utils import check_X_y
+from sklearn.utils.multiclass import type_of_target
 from sklearn.base import clone
 from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
@@ -127,9 +128,19 @@ class DoubleMLIRM(DoubleML):
         return score
 
     def _check_data(self, obj_dml_data):
-        assert obj_dml_data.z_cols is None
-        assert obj_dml_data.n_treat == 1
-        check_binary_vector(obj_dml_data.d, variable_name='d')
+        if obj_dml_data.z_cols is not None:
+            raise ValueError('Incompatible data.\n'
+                             ' and '.join(obj_dml_data.z_cols) +
+                             'have been set as instrumental variable(s).\n'
+                             'To fit an interactiv IV regression model use DoubleMLIIVM instead of DoubleMLIRM.')
+        one_treat = (obj_dml_data.n_treat == 1)
+        binary_treat = (type_of_target(obj_dml_data.d) == 'binary')
+        zero_one_treat = np.any((np.power(obj_dml_data.d, 2) - obj_dml_data.d) == 0)
+        if not(one_treat & binary_treat & zero_one_treat):
+            raise ValueError('Incompatible data.\n'
+                             'To fit an IIVM model with DML '
+                             'exactly one binary variable with values 0 and 1 '
+                             'needs to be specified as treatment variable.')
         return
     
     def _get_cond_smpls(self, smpls, d):

doubleml/_helper.py
@@ -17,14 +17,6 @@ def assure_2d_array(x):
     return x
 
 
-def check_binary_vector(x, variable_name=''):
-    # assure D binary
-    assert type_of_target(x) == 'binary', 'variable ' + variable_name  + ' must be binary'
-    
-    if np.any(np.power(x, 2) - x != 0):
-        raise ValueError('variable ' + variable_name + ' must be binary with values 0 and 1')
-
-
 def _check_is_partition(smpls, n_obs):
     test_indices = np.concatenate([test_index for _, test_index in smpls])
     if len(test_indices) != n_obs:

doubleml/double_ml_data.py
@@ -208,7 +208,11 @@ class DoubleMLData:
         """
         The number of instruments.
         """
-        return len(self.z_cols)
+        if self.z_cols is not None:
+            n_instr = len(self.z_cols)
+        else:
+            n_instr = 0
+        return n_instr
     
     @property 
     def n_obs(self):

doubleml/double_ml_plr.py
@@ -107,7 +107,11 @@ class DoubleMLPLR(DoubleML):
         return score
 
     def _check_data(self, obj_dml_data):
-        assert obj_dml_data.z_cols is None, 'incompatible data provided: PLR model has no instrumental variable Z'
+        if obj_dml_data.z_cols is not None:
+            raise ValueError('Incompatible data.\n'
+                             ' and '.join(obj_dml_data.z_cols) +
+                             'have been set as instrumental variable(s).\n'
+                             'To fit an partially linear IV regression model use DoubleMLPLIV instead of DoubleMLPLR.')
         return
 
     def _ml_nuisance_and_score_elements(self, smpls, n_jobs_cv):

commit f62373e50aa1a07da4b976b27a23e93745f5830c

fix bug introduced in zero one binary vector check introduced in 8cbb86900c9d39a1783fd62e3f25096403ed635a

doubleml/double_ml_iivm.py
@@ -133,7 +133,7 @@ class DoubleMLIIVM(DoubleML):
     def _check_data(self, obj_dml_data):
         one_treat = (obj_dml_data.n_treat == 1)
         binary_treat = (type_of_target(obj_dml_data.d) == 'binary')
-        zero_one_treat = np.any((np.power(obj_dml_data.d, 2) - obj_dml_data.d) == 0)
+        zero_one_treat = np.all((np.power(obj_dml_data.d, 2) - obj_dml_data.d) == 0)
         if not(one_treat & binary_treat & zero_one_treat):
             raise ValueError('Incompatible data.\n'
                              'To fit an IIVM model with DML '
@@ -141,7 +141,7 @@ class DoubleMLIIVM(DoubleML):
                              'needs to be specified as treatment variable.')
         one_instr = (obj_dml_data.n_instr == 1)
         binary_instr = (type_of_target(obj_dml_data.z) == 'binary')
-        zero_one_instr = np.any((np.power(obj_dml_data.z, 2) - obj_dml_data.z) == 0)
+        zero_one_instr = np.all((np.power(obj_dml_data.z, 2) - obj_dml_data.z) == 0)
         if not(one_instr & binary_instr & zero_one_instr):
             raise ValueError('Incompatible data.\n'
                              'To fit an IIVM model with DML '

doubleml/double_ml_irm.py
@@ -130,7 +130,7 @@ class DoubleMLIRM(DoubleML):
                              'To fit an interactive IV regression model use DoubleMLIIVM instead of DoubleMLIRM.')
         one_treat = (obj_dml_data.n_treat == 1)
         binary_treat = (type_of_target(obj_dml_data.d) == 'binary')
-        zero_one_treat = np.any((np.power(obj_dml_data.d, 2) - obj_dml_data.d) == 0)
+        zero_one_treat = np.all((np.power(obj_dml_data.d, 2) - obj_dml_data.d) == 0)
         if not(one_treat & binary_treat & zero_one_treat):
             raise ValueError('Incompatible data. '
                              'To fit an IRM model with DML '