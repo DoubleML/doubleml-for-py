cc2dba1c95e403a6dfba0bac8eda2bd18d1720e0
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-02-01 16:48:33 +0100

    added unit tests for the set_sample_splitting method

---
+++ doubleml/tests/test_doubleml_set_sample_splitting.py
@@ -0,0 +1,145 @@
+import pytest
+import numpy as np
+
+from doubleml import DoubleMLPLR, DoubleMLIRM
+from doubleml.datasets import make_plr_CCDDHNR2018, make_irm_data
+
+from sklearn.linear_model import Lasso, LogisticRegression
+from sklearn.base import BaseEstimator
+
+dml_data = make_plr_CCDDHNR2018(n_obs=10)
+ml_g = Lasso()
+ml_m = Lasso()
+dml_plr = DoubleMLPLR(dml_data, ml_g, ml_m,
+                      n_folds=13, n_rep=13,
+                      draw_sample_splitting=False)
+
+
+@pytest.mark.ci
+def test_doubleml_set_sample_splitting_tuple():
+    # simple sample splitting with two folds and without cross-fitting
+    smpls = ([0, 1, 2, 3, 4], [5, 6, 7, 8, 9])
+    dml_plr.set_sample_splitting(smpls)
+
+    assert np.array_equal(dml_plr.smpls[0][0], smpls)
+    assert dml_plr.n_folds == 2
+    assert dml_plr.n_rep == 1
+    assert not dml_plr.apply_cross_fitting
+
+    # no cross-fitting, no sample-splitting
+    smpls = ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    dml_plr.set_sample_splitting(smpls)
+    assert dml_plr.n_folds == 1
+    assert dml_plr.n_rep == 1
+    assert not dml_plr.apply_cross_fitting
+
+    smpls = ([0, 1, 2, 3, 4], [5, 6], [7, 8, 9])
+    msg = 'Invalid partition provided. ' + 'Tuple for train_ind and test_ind must consist of exactly two elements.'
+    with pytest.raises(ValueError, match=msg):
+        dml_plr.set_sample_splitting(smpls)
+
+
+@pytest.mark.ci
+def test_doubleml_set_sample_splitting_all_tuple():
+    # sample splitting with two folds and cross-fitting but no repeated cross-fitting
+    smpls = [([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]),
+             ([5, 6, 7, 8, 9], [0, 1, 2, 3, 4])]
+    dml_plr.set_sample_splitting(smpls)
+    assert dml_plr.n_folds == 2
+    assert dml_plr.n_rep == 1
+    assert dml_plr.apply_cross_fitting
+
+    smpls = [([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]),
+             ([5, 6, 7, 8, 9], [0, 1, 2], [3, 4])]
+    msg = 'Invalid partition provided. ' + 'All tuples for train_ind and test_ind must consist of exactly two elements.'
+    with pytest.raises(ValueError, match=msg):
+        dml_plr.set_sample_splitting(smpls)
+
+    # simple sample splitting with two folds and without cross-fitting
+    smpls = [([0, 1, 2, 3, 4], [5, 6, 7, 8, 9])]
+    dml_plr.set_sample_splitting(smpls)
+
+    assert np.array_equal(dml_plr.smpls[0], smpls)
+    assert dml_plr.n_folds == 2
+    assert dml_plr.n_rep == 1
+    assert not dml_plr.apply_cross_fitting
+
+    # sample splitting with cross-fitting and two folds that do not form a partition
+    smpls = [([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]),
+             ([5, 6, 7, 8], [0, 1, 2, 3, 4, 9])]
+    msg = 'Invalid partition provided. ' + 'Tuples for more than one fold provided that don\'t form a partition.'
+    with pytest.raises(ValueError, match=msg):
+        dml_plr.set_sample_splitting(smpls)
+
+
+@pytest.mark.ci
+def test_doubleml_set_sample_splitting_all_list():
+    # sample splitting with two folds and repeated cross-fitting with n_rep = 2
+    smpls = [[([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]),
+              ([5, 6, 7, 8, 9], [0, 1, 2, 3, 4])],
+             [([0, 2, 4, 6, 8], [1, 3, 5, 7, 9]),
+              ([1, 3, 5, 7, 9], [0, 2, 4, 6, 8])]]
+    dml_plr.set_sample_splitting(smpls)
+    assert dml_plr.n_folds == 2
+    assert dml_plr.n_rep == 2
+    assert dml_plr.apply_cross_fitting
+
+    smpls = np.array(([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]))
+    # msg = ("all_smpls must be of list or tuple type. [[0 1 2 3 4]"
+    #        r"\n [5 6 7 8 9]] of type <class 'numpy.ndarray'> was passed.")
+    with pytest.raises(TypeError):
+        dml_plr.set_sample_splitting(smpls)
+
+    # second sample splitting is not a list
+    smpls = [[([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]),
+              ([5, 6, 7, 8, 9], [0, 1, 2, 3, 4])],
+             (([0, 2, 4, 6, 8], [1, 3, 5, 7, 9]),
+              ([1, 3, 5, 7, 9], [0, 2, 4, 6, 8]))]
+    msg = ('Invalid partition provided. '
+           'all_smpls is a list where neither all elements are tuples nor all elements are lists.')
+    with pytest.raises(ValueError, match=msg):
+        dml_plr.set_sample_splitting(smpls)
+
+    smpls = [[([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]),
+              ([5, 6, 7, 8, 9], [0, 1, 2, 3, 4])],
+             [[[0, 2, 4, 6, 8], [1, 3, 5, 7, 9]],  # not a tuple
+              ([1, 3, 5, 7, 9], [0, 2, 4, 6, 8])]]
+    msg = 'For repeated sample splitting all_smpls must be list of lists of tuples.'
+    with pytest.raises(TypeError, match=msg):
+        dml_plr.set_sample_splitting(smpls)
+
+    smpls = [[([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]),
+              ([5, 6, 7, 8, 9], [0, 1, 2, 3, 4])],
+             [([0, 2, 4, 6, 8], [1, 3, 5, 7, 9]),
+              ([1, 3, 5, 7, 9], [0, 2, 4], [6, 8])]]
+    msg = 'Invalid partition provided. ' + 'All tuples for train_ind and test_ind must consist of exactly two elements.'
+    with pytest.raises(ValueError, match=msg):
+        dml_plr.set_sample_splitting(smpls)
+
+    smpls = [[([0, 1, 2, 3, 6], [4, 5, 7, 8, 9]),
+              ([4, 5, 7, 8, 9], [0, 1, 2, 3, 6])],
+             [([0, 1, 4, 5, 7, 9], [2, 3, 6, 8]),
+              ([0, 2, 3, 4, 6, 8, 9], [1, 5, 7]),
+              ([1, 2, 3, 5, 6, 7, 8], [0, 4, 9])]]
+    msg = 'Invalid partition provided. ' + 'Different number of folds for repeated sample splitting.'
+    with pytest.raises(ValueError, match=msg):
+        dml_plr.set_sample_splitting(smpls)
+
+    # sample splitting with cross-fitting and two folds that do not form a partition
+    smpls = [[([0, 1, 2, 3, 4], [5, 6, 7, 8, 9]),
+              ([5, 6, 7, 8], [0, 1, 2, 3, 4, 9])]]
+    msg = ('Invalid partition provided. '
+           'Tuples for more than one fold provided '
+           'but at least one does not form a partition.')
+    with pytest.raises(ValueError, match=msg):
+        dml_plr.set_sample_splitting(smpls)
+
+    # repeated no-cross-fitting
+    smpls = [[([0, 1, 5, 7, 9], [2, 3, 4, 6, 8])],
+             [([2, 4, 7, 8, 9], [0, 1, 3, 5, 6])],
+             [([0, 1, 4, 6, 8], [2, 3, 5, 7, 9])]]
+    dml_plr.set_sample_splitting(smpls)
+    assert dml_plr.n_folds == 2
+    assert dml_plr.n_rep == 3
+    assert not dml_plr.apply_cross_fitting
+
