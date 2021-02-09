e6c0219c4f403887fa99491e909fe27899c8c607
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-02-09 09:49:18 +0100

    use double backslash in docstring math

--- doubleml/double_ml_irm.py
+++ doubleml/double_ml_irm.py
@@ -77,23 +77,23 @@ class DoubleMLIRM(DoubleML):
 
     .. math::
 
-        Y = g_0(D, X) + U, & &\mathbb{E}(U | X, D) = 0,
+        Y = g_0(D, X) + U, & &\\mathbb{E}(U | X, D) = 0,
 
-        D = m_0(X) + V, & &\mathbb{E}(V | X) = 0,
+        D = m_0(X) + V, & &\\mathbb{E}(V | X) = 0,
 
-    where the treatment variable is binary, :math:`D \in \lbrace 0,1 \rbrace`.
+    where the treatment variable is binary, :math:`D \\in \\lbrace 0,1 \\rbrace`.
     We consider estimation of the average treatment effects when treatment effects are fully heterogeneous.
     Target parameters of interest in this model are the average treatment effect (ATE),
 
     .. math::
 
-        \theta_0 = \mathbb{E}[g_0(1, X) - g_0(0,X)]
+        \\theta_0 = \\mathbb{E}[g_0(1, X) - g_0(0,X)]
 
     and the average treatment effect of the treated (ATTE),
 
     .. math::
 
-        \theta_0 = \mathbb{E}[g_0(1, X) - g_0(0,X) | D=1].
+        \\theta_0 = \\mathbb{E}[g_0(1, X) - g_0(0,X) | D=1].
     """
     def __init__(self,
                  obj_dml_data,
