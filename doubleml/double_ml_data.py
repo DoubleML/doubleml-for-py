df276a99fcc12abd30a065c64063a5291c1c2539
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-04-16 13:12:54 +0200

    check for duplicate entries in x_cols, d_cols and z_cols

--- doubleml/double_ml_data.py
+++ doubleml/double_ml_data.py
@@ -246,6 +246,9 @@ class DoubleMLData:
             if not isinstance(value, list):
                 raise TypeError('The covariates x_cols must be of str or list type (or None). '
                                 f'{str(value)} of type {str(type(value))} was passed.')
+            if not len(set(value)) == len(value):
+                raise ValueError('Invalid covariates x_cols: '
+                                 'Contains duplicate values.')
             if not set(value).issubset(set(self.all_variables)):
                 raise ValueError('Invalid covariates x_cols. '
                                  'At least one covariate is no data column.')
@@ -280,6 +283,9 @@ class DoubleMLData:
         if not isinstance(value, list):
             raise TypeError('The treatment variable(s) d_cols must be of str or list type. '
                             f'{str(value)} of type {str(type(value))} was passed.')
+        if not len(set(value)) == len(value):
+            raise ValueError('Invalid treatment variable(s) d_cols: '
+                             'Contains duplicate values.')
         if not set(value).issubset(set(self.all_variables)):
             raise ValueError('Invalid treatment variable(s) d_cols. '
                              'At least one treatment variable is no data column.')
@@ -326,6 +332,9 @@ class DoubleMLData:
             if not isinstance(value, list):
                 raise TypeError('The instrumental variable(s) z_cols must be of str or list type (or None). '
                                 f'{str(value)} of type {str(type(value))} was passed.')
+            if not len(set(value)) == len(value):
+                raise ValueError('Invalid instrumental variable(s) z_cols: '
+                                 'Contains duplicate values.')
             if not set(value).issubset(set(self.all_variables)):
                 raise ValueError('Invalid instrumental variable(s) z_cols. '
                                  'At least one instrumental variable is no data column.')
