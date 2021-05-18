8f5bae4a0fd75519a6f649644a27f9549857f191
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-05-18 14:23:02 +0200

    variable i_rep is not used

--- doubleml/tests/_utils.py
+++ doubleml/tests/_utils.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import KFold, GridSearchCV
 
 def draw_smpls(n_obs, n_folds, n_rep=1):
     all_smpls = []
-    for i_rep in range(n_rep):
+    for _ in range(n_rep):
         resampling = KFold(n_splits=n_folds,
                            shuffle=True)
         smpls = [(train, test) for train, test in resampling.split(np.zeros(n_obs))]
