2e8e720c611b71dd96805ee66758f2a67490a036
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-07-26 16:27:55 +0200

    updated the not-yet-implemented unit tests

--- doubleml/tests/test_doubleml_exceptions.py
+++ doubleml/tests/test_doubleml_exceptions.py
@@ -475,19 +475,15 @@ def test_doubleml_cluster_not_yet_implemented():
     dml_cluster_data_multiway = DoubleMLClusterData(df, y_col='Y', d_cols='D', x_cols=['X1', 'X5'], z_cols='Z',
                                                     cluster_cols=['cluster_var_i', 'cluster_var_j', 'cluster_var_k'])
     assert dml_cluster_data_multiway.n_cluster_vars == 3
-    msg = r'Multi-way \(n_ways > 2\) clustering not yet implemented with clustering.'
+    msg = r'Multi-way \(n_ways > 2\) clustering not yet implemented.'
     with pytest.raises(NotImplementedError, match=msg):
         _ = DoubleMLPLIV(dml_cluster_data_multiway, ml_g, ml_m, ml_r)
 
-    msg = (r'Repeated cross-fitting \(`n_rep > 1`\) '
-           r'and no cross-fitting \(`apply_cross_fitting = False`\) '
-           'are not yet implemented with clustering.')
+    msg = (r'No cross-fitting \(`apply_cross_fitting = False`\) '
+           'is not yet implemented with clustering.')
     with pytest.raises(NotImplementedError, match=msg):
         _ = DoubleMLPLIV(dml_cluster_data_pliv, ml_g, ml_m, ml_r,
                          n_folds=1)
     with pytest.raises(NotImplementedError, match=msg):
         _ = DoubleMLPLIV(dml_cluster_data_pliv, ml_g, ml_m, ml_r,
                          apply_cross_fitting=False, n_folds=2)
-    with pytest.raises(NotImplementedError, match=msg):
-        _ = DoubleMLPLIV(dml_cluster_data_pliv, ml_g, ml_m, ml_r,
-                         n_rep=10)
