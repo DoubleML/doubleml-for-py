5417798a3a3b33f49e2e39c60b7325c5811f52d9
Malte S. Kurz <malte.s.kurz@gmail.com>
2021-10-13 17:19:18 +0200

    update copyright year

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'DoubleML'
-copyright = '2020, Bach, P., Chernozhukov, V., Kurz, M. S., and Spindler, M.'
+copyright = '2021, Bach, P., Chernozhukov, V., Kurz, M. S., and Spindler, M.'
 author = 'Bach, P., Chernozhukov, V., Kurz, M. S., and Spindler, M.'
 
 # The full version, including alpha/beta/rc tags
