b4a40103422e68c85ac9694252016fd26d3c5360
Riera Auge <bbb1675@uni-hamburg.de>
2022-03-23 12:54:21 +0100

    added missing package

--- doubleml/double_ml_irm.py
+++ doubleml/double_ml_irm.py
@@ -1,5 +1,6 @@
 import patsy
 import numpy as np
+import statsmodels.api as sm
 from sklearn.utils import check_X_y
 from sklearn.utils.multiclass import type_of_target
 from sklearn.preprocessing import PolynomialFeatures
