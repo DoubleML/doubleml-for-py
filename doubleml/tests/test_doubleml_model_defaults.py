ecb36b907244d52200582eae02a93504ece9dc76
Shrey Dixit <shreyakshaj@gmail.com>
2022-04-07 13:28:23 +0200

    Added tests for defaults

--- doubleml/tests/test_doubleml_model_defaults.py
+++ doubleml/tests/test_doubleml_model_defaults.py
@@ -1,17 +1,19 @@
 import pytest
 import numpy as np
 
-from doubleml import DoubleMLPLR, DoubleMLIRM, DoubleMLIIVM, DoubleMLPLIV
-from doubleml.datasets import make_plr_CCDDHNR2018, make_irm_data, make_pliv_CHS2015, make_iivm_data
+from doubleml import DoubleMLPLR, DoubleMLIRM, DoubleMLIIVM, DoubleMLPLIV, DoubleMLDiD
+from doubleml.datasets import make_diff_in_diff_chang2020, make_plr_CCDDHNR2018, make_irm_data, make_pliv_CHS2015, make_iivm_data
 
 from sklearn.linear_model import Lasso, LogisticRegression
 
 np.random.seed(3141)
+dml_data_did_ro = make_diff_in_diff_chang2020(n_obs=10)
 dml_data_plr = make_plr_CCDDHNR2018(n_obs=100)
 dml_data_pliv = make_pliv_CHS2015(n_obs=100, dim_z=1)
 dml_data_irm = make_irm_data(n_obs=100)
 dml_data_iivm = make_iivm_data(n_obs=100)
 
+dml_did_ro = DoubleMLDiD(dml_data_did_ro, Lasso(), LogisticRegression())
 dml_plr = DoubleMLPLR(dml_data_plr, Lasso(), Lasso())
 dml_pliv = DoubleMLPLIV(dml_data_pliv, Lasso(), Lasso(), Lasso())
 dml_irm = DoubleMLIRM(dml_data_irm, Lasso(), LogisticRegression())
@@ -58,3 +60,14 @@ def test_iivm_defaults():
     assert dml_iivm.dml_procedure == 'dml2'
     assert dml_iivm.trimming_rule == 'truncate'
     assert dml_iivm.trimming_threshold == 1e-12
+
+
+@pytest.mark.ci
+def test_did_defaults():
+    _assert_resampling_default_settings(dml_did_ro)
+    assert dml_did_ro.n_folds == 5
+    assert dml_did_ro.n_rep == 1
+    assert dml_did_ro.score == 'ortho_ro'
+    assert dml_did_ro.dml_procedure == 'dml2'
+    assert dml_did_ro.trimming_rule == 'truncate'
+    assert dml_did_ro.trimming_threshold == 1e-12
