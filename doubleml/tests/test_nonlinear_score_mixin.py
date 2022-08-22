fd799a7da62dfa3e048c9eea7abc8ef14582af4c
Malte S. Kurz <malte.s.kurz@gmail.com>
2022-08-22 17:25:53 +0200

    fix the nonlinear score mixin unit test after merge

--- doubleml/tests/test_nonlinear_score_mixin.py
+++ doubleml/tests/test_nonlinear_score_mixin.py
@@ -19,8 +19,9 @@ class DoubleMLPLRWithNonLinearScoreMixin(NonLinearScoreMixin, DoubleML):
 
     def __init__(self,
                  obj_dml_data,
-                 ml_g,
+                 ml_l,
                  ml_m,
+                 ml_g=None,
                  n_folds=5,
                  n_rep=1,
                  score='partialling out',
@@ -37,10 +38,17 @@ class DoubleMLPLRWithNonLinearScoreMixin(NonLinearScoreMixin, DoubleML):
 
         self._check_data(self._dml_data)
         self._check_score(self.score)
-        _ = self._check_learner(ml_g, 'ml_g', regressor=True, classifier=False)
+
+        _ = self._check_learner(ml_l, 'ml_l', regressor=True, classifier=False)
         _ = self._check_learner(ml_m, 'ml_m', regressor=True, classifier=False)
-        self._learner = {'ml_g': ml_g, 'ml_m': ml_m}
-        self._predict_method = {'ml_g': 'predict', 'ml_m': 'predict'}
+        self._learner = {'ml_l': ml_l, 'ml_m': ml_m}
+        self._predict_method = {'ml_l': 'predict',
+                                'ml_m': 'predict'}
+
+        if ml_g is not None:
+            _ = self._check_learner(ml_g, 'ml_g', regressor=True, classifier=False)
+            self._learner['ml_g'] = ml_g
+            self._predict_method['ml_g'] = 'predict'
 
         self._initialize_ml_nuisance_params()
 
@@ -64,7 +72,8 @@ class DoubleMLPLRWithNonLinearScoreMixin(NonLinearScoreMixin, DoubleML):
         return psi_a
 
     def _initialize_ml_nuisance_params(self):
-        self._params = {learner: {key: [None] * self.n_rep for key in self._dml_data.d_cols} for learner in ['ml_g', 'ml_m']}
+        self._params = {learner: {key: [None] * self.n_rep for key in self._dml_data.d_cols}
+                        for learner in self._learner}
 
     def _check_score(self, score):
         pass
@@ -72,48 +81,60 @@ class DoubleMLPLRWithNonLinearScoreMixin(NonLinearScoreMixin, DoubleML):
     def _check_data(self, obj_dml_data):
         pass
 
-    def _ml_nuisance_and_score_elements(self, smpls, n_jobs_cv):
+    def _nuisance_est(self, smpls, n_jobs_cv):
         x, y = check_X_y(self._dml_data.x, self._dml_data.y,
                          force_all_finite=False)
         x, d = check_X_y(x, self._dml_data.d,
                          force_all_finite=False)
 
-        # nuisance g
-        g_hat = _dml_cv_predict(self._learner['ml_g'], x, y, smpls=smpls, n_jobs=n_jobs_cv,
-                                est_params=self._get_params('ml_g'), method=self._predict_method['ml_g'])
-        _check_finite_predictions(g_hat, self._learner['ml_g'], 'ml_g', smpls)
+        # nuisance l
+        l_hat = _dml_cv_predict(self._learner['ml_l'], x, y, smpls=smpls, n_jobs=n_jobs_cv,
+                                est_params=self._get_params('ml_l'), method=self._predict_method['ml_l'])
+        _check_finite_predictions(l_hat, self._learner['ml_l'], 'ml_l', smpls)
 
         # nuisance m
         m_hat = _dml_cv_predict(self._learner['ml_m'], x, d, smpls=smpls, n_jobs=n_jobs_cv,
                                 est_params=self._get_params('ml_m'), method=self._predict_method['ml_m'])
         _check_finite_predictions(m_hat, self._learner['ml_m'], 'ml_m', smpls)
 
-        psi_a, psi_b = self._score_elements(y, d, g_hat, m_hat, smpls)
+        # an estimate of g is obtained for the IV-type score and callable scores
+        g_hat = None
+        if 'ml_g' in self._learner:
+            # get an initial estimate for theta using the partialling out score
+            psi_a = -np.multiply(d - m_hat, d - m_hat)
+            psi_b = np.multiply(d - m_hat, y - l_hat)
+            theta_initial = -np.nanmean(psi_b) / np.nanmean(psi_a)
+            # nuisance g
+            g_hat = _dml_cv_predict(self._learner['ml_g'], x, y - theta_initial*d, smpls=smpls, n_jobs=n_jobs_cv,
+                                    est_params=self._get_params('ml_g'), method=self._predict_method['ml_g'])
+            _check_finite_predictions(g_hat, self._learner['ml_g'], 'ml_g', smpls)
+
+        psi_a, psi_b = self._score_elements(y, d, l_hat, m_hat, g_hat, smpls)
         psi_elements = {'psi_a': psi_a,
                         'psi_b': psi_b}
-        preds = {'ml_g': g_hat,
-                 'ml_m': m_hat}
+        preds = {'ml_l': l_hat,
+                 'ml_m': m_hat,
+                 'ml_g': g_hat}
 
         return psi_elements, preds
 
-    def _score_elements(self, y, d, g_hat, m_hat, smpls):
+    def _score_elements(self, y, d, l_hat, m_hat, g_hat, smpls):
         # compute residuals
-        u_hat = y - g_hat
+        u_hat = y - l_hat
         v_hat = d - m_hat
-        v_hatd = np.multiply(v_hat, d)
 
-        assert isinstance(self.score, str)
         if self.score == 'IV-type':
-            psi_a = -v_hatd
+            psi_a = - np.multiply(v_hat, d)
+            psi_b = np.multiply(v_hat, y - g_hat)
         else:
             assert self.score == 'partialling out'
             psi_a = -np.multiply(v_hat, v_hat)
-        psi_b = np.multiply(v_hat, u_hat)
+            psi_b = np.multiply(v_hat, u_hat)
 
         return psi_a, psi_b
 
-    def _ml_nuisance_tuning(self, smpls, param_grids, scoring_methods, n_folds_tune, n_jobs_cv,
-                            search_mode, n_iter_randomized_search):
+    def _nuisance_tuning(self, smpls, param_grids, scoring_methods, n_folds_tune, n_jobs_cv,
+                         search_mode, n_iter_randomized_search):
         pass
 
 
@@ -150,26 +171,44 @@ def dml_plr_w_nonlinear_mixin_fixture(generate_data1, learner, score, dml_proced
     data = generate_data1
     x_cols = data.columns[data.columns.str.startswith('X')].tolist()
 
-    # Set machine learning methods for m & g
-    ml_g = clone(learner)
+    # Set machine learning methods for l, m & g
+    ml_l = clone(learner)
     ml_m = clone(learner)
+    ml_g = clone(learner)
 
     np.random.seed(3141)
     obj_dml_data = dml.DoubleMLData(data, 'y', ['d'], x_cols)
-    dml_plr_obj = dml.DoubleMLPLR(obj_dml_data,
-                                  ml_g, ml_m,
-                                  n_folds=n_folds,
-                                  score=score,
-                                  dml_procedure=dml_procedure)
+    if score == 'partialling out':
+        dml_plr_obj = dml.DoubleMLPLR(obj_dml_data,
+                                      ml_l, ml_m,
+                                      n_folds=n_folds,
+                                      score=score,
+                                      dml_procedure=dml_procedure)
+    else:
+        assert score == 'IV-type'
+        dml_plr_obj = dml.DoubleMLPLR(obj_dml_data,
+                                      ml_l, ml_m, ml_g,
+                                      n_folds,
+                                      score=score,
+                                      dml_procedure=dml_procedure)
 
     dml_plr_obj.fit()
 
     np.random.seed(3141)
-    dml_plr_obj2 = DoubleMLPLRWithNonLinearScoreMixin(obj_dml_data,
-                                                      ml_g, ml_m,
-                                                      n_folds=n_folds,
-                                                      score=score,
-                                                      dml_procedure=dml_procedure)
+    if score == 'partialling out':
+        dml_plr_obj2 = DoubleMLPLRWithNonLinearScoreMixin(obj_dml_data,
+                                                          ml_l, ml_m,
+                                                          n_folds=n_folds,
+                                                          score=score,
+                                                          dml_procedure=dml_procedure)
+    else:
+        assert score == 'IV-type'
+        dml_plr_obj2 = DoubleMLPLRWithNonLinearScoreMixin(obj_dml_data,
+                                                          ml_l, ml_m, ml_g,
+                                                          n_folds,
+                                                          score=score,
+                                                          dml_procedure=dml_procedure)
+
     dml_plr_obj2._coef_bounds = coef_bounds  # use different settings to also unit test the solver for bounded problems
     dml_plr_obj2.fit()
 
@@ -200,9 +239,9 @@ def test_doubleml_cluster_not_implemented_exception():
     np.random.seed(3141)
     dml_data = make_pliv_multiway_cluster_CKMS2021()
     dml_data.z_cols = None
-    ml_g = LinearRegression()
+    ml_l = LinearRegression()
     ml_m = LinearRegression()
-    dml_plr = DoubleMLPLRWithNonLinearScoreMixin(dml_data, ml_g, ml_m)
+    dml_plr = DoubleMLPLRWithNonLinearScoreMixin(dml_data, ml_l, ml_m)
     msg = 'Estimation with clustering not implemented.'
     with pytest.raises(NotImplementedError, match=msg):
         _ = dml_plr.fit()
