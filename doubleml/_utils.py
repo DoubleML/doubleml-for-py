c8461cdc941cbe9c4127a9784e03c22457a924a0
Sven Klaassen <47529404+SvenKlaassen@users.noreply.github.com>
2023-01-20 11:07:15 +0100

    correct normalize weights

--- doubleml/_utils.py
+++ doubleml/_utils.py
@@ -224,8 +224,9 @@ def _trimm(preds, trimming_rule, trimming_threshold):
 
 
 def _normalize_ipw(propensity, treatment):
-    norm_factor = np.multiply(treatment, np.mean(np.divide(treatment, propensity))) \
-        + np.multiply(1.0-treatment, np.mean(np.divide(1.0-treatment, 1.0-propensity)))
+    mean_treat1 = np.mean(np.divide(treatment, propensity))
+    mean_treat0 = np.mean(np.divide(1.0-treatment, 1.0-propensity))
+    normalized_weights = np.multiply(treatment, np.multiply(propensity, mean_treat1)) \
+        + np.multiply(1.0-treatment, 1.0 - np.multiply(1.0-propensity, mean_treat0))
 
-    propensity *= norm_factor
-    return propensity
+    return normalized_weights
