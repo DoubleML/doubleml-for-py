a70e5a8784bedc8a5d25c77d8486123253545afd
Sven Klaassen <47529404+SvenKlaassen@users.noreply.github.com>
2023-01-20 12:22:28 +0100

    fix format

--- doubleml/_utils.py
+++ doubleml/_utils.py
@@ -239,4 +239,3 @@ def _check_is_propensity(preds, learner, learner_name, smpls, eps=1e-12):
         warnings.warn(f'Propensity predictions from learner {str(learner)} for'
                       f' {learner_name} are close to zero or one (eps={eps}).')
     return
-
