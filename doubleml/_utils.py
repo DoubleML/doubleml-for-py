fix rmse calculation and learner evaluation for IRM and IIVM models

doubleml/_utils.py
@@ -6,6 +6,7 @@ from sklearn.base import clone
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.metrics import mean_squared_error
 
 from joblib import Parallel, delayed
 
@@ -111,7 +112,7 @@ def _dml_cv_predict(estimator, x, y, smpls=None,
             res['preds'] = preds[:, 1]
         else:
             res['preds'] = preds
-        res['targets'] = y
+        res['targets'] = np.copy(y)
     else:
         if not smpls_is_partition:
             assert not fold_specific_target, 'combination of fold-specific y and no cross-fitting not implemented yet'
@@ -235,3 +236,9 @@ def _check_is_propensity(preds, learner, learner_name, smpls, eps=1e-12):
         warnings.warn(f'Propensity predictions from learner {str(learner)} for'
                       f' {learner_name} are close to zero or one (eps={eps}).')
     return
+
+
+def _rmse(y_true, y_pred):
+    subset = np.logical_not(np.isnan(y_true))
+    rmse = mean_squared_error(y_true[subset], y_pred[subset], squared=False)
+    return rmse

doubleml/double_ml.py
@@ -3,7 +3,6 @@ import pandas as pd
 import warnings
 
 from sklearn.base import is_regressor, is_classifier
-from sklearn.metrics import mean_squared_error
 
 from scipy.stats import norm
 
@@ -13,7 +12,8 @@ from abc import ABC, abstractmethod
 
 from .double_ml_data import DoubleMLBaseData, DoubleMLClusterData
 from ._utils_resampling import DoubleMLResampling, DoubleMLClusterResampling
-from ._utils import _check_is_partition, _check_all_smpls, _check_smpl_split, _check_smpl_split_tpl, _draw_weights
+from ._utils import _check_is_partition, _check_all_smpls, _check_smpl_split, _check_smpl_split_tpl, _draw_weights, \
+    _rmse
 
 
 _implemented_data_backends = ['DoubleMLData', 'DoubleMLClusterData']
@@ -1042,13 +1042,13 @@ class DoubleML(ABC):
                 self._rmses[learner][self._i_rep, self._i_treat] = np.nan
             else:
                 sq_error = np.power(targets[learner] - preds[learner], 2)
-                self._rmses[learner][self._i_rep, self._i_treat] = np.sqrt(np.mean(sq_error, 0))
+                self._rmses[learner][self._i_rep, self._i_treat] = np.sqrt(np.nanmean(sq_error, axis=0))
 
     def _store_models(self, models):
         for learner in self.params_names:
             self._models[learner][self._dml_data.d_cols[self._i_treat]][self._i_rep] = models[learner]
 
-    def evaluate_learners(self, learners=None, metric=mean_squared_error):
+    def evaluate_learners(self, learners=None, metric=_rmse):
         """
        Evaluate fitted learners for DoubleML models on cross-validated predictions.
 
@@ -1059,7 +1059,9 @@ class DoubleML(ABC):
 
         metric : callable
             A callable function with inputs ``y_pred`` and ``y_true`` of shape ``(1, n)``,
-            where ``n`` specifies the number of observations.
+            where ``n`` specifies the number of observations. Remark that some models like IRM are
+            not able to provide all values for ``y_true`` for all learners and might contain
+            some ``nan`` values in the target vector.
             Default is the euclidean distance.
 
         Returns