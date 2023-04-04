cd01a8b261fd4f0f4344a7a458fc56961327406a
SvenKlaassen <47529404+SvenKlaassen@users.noreply.github.com>
2023-04-04 14:18:42 +0200

    change Licence to BSD 3-Clause License

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
     ],
 )
