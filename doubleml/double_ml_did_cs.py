14c8674c9a6be711ca9c41562668ca2d7c31c4b2
Sven Klaassen <47529404+SvenKlaassen@users.noreply.github.com>
2023-04-19 12:18:39 +0200

    update documentation

--- doubleml/double_ml_did_cs.py
+++ doubleml/double_ml_did_cs.py
@@ -20,7 +20,7 @@ class DoubleMLDIDCS(LinearScoreMixin, DoubleML):
 
     ml_g : estimator implementing ``fit()`` and ``predict()``
         A machine learner implementing ``fit()`` and ``predict()`` methods (e.g.
-        :py:class:`sklearn.ensemble.RandomForestRegressor`) for the nuisance function :math:`g_0(D,X) = E[Y|X,D]`.
+        :py:class:`sklearn.ensemble.RandomForestRegressor`) for the nuisance function :math:`g_0(d,t,X) = E[Y|D=d,T=t,X]`.
         For a binary outcome variable :math:`Y` (with values 0 and 1), a classifier implementing ``fit()`` and
         ``predict_proba()`` can also be specified. If :py:func:`sklearn.base.is_classifier` returns ``True``,
         ``predict_proba()`` is used otherwise ``predict()``.
