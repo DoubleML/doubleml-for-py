509073a614a221c22eb6ea682499bc9a1fe1f583
Sven Klaassen <47529404+SvenKlaassen@users.noreply.github.com>
2023-06-26 13:23:33 +0200

    update release number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ PROJECT_URLS = {
 
 setup(
     name='DoubleML',
-    version='0.6.2',
+    version='0.6.3',
     author='Bach, P., Chernozhukov, V., Kurz, M. S., and Spindler, M.',
     maintainer='Malte S. Kurz',
     maintainer_email='malte.simon.kurz@uni-hamburg.de',
