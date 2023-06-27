f72d6b984fd6e93a82a1d0700caab77f8b2f136e
Jan Teichert-Kluge <jan.teichertkluge@uni-hamburg.de>
2023-06-27 15:59:42 +0200

    Fix testcases for external predictions

--- doubleml/tests/test_external_predictions.py
+++ doubleml/tests/test_external_predictions.py
@@ -5,11 +5,17 @@ from sklearn.linear_model import LinearRegression, LassoCV
 from doubleml import DoubleMLPLR, DoubleMLData
 from doubleml.datasets import make_plr_CCDDHNR2018
 
-# @pytest.fixture(scope='module',
-#                 params=[LinearRegression(),
-#                         LassoCV()])
-# def learner(request):
-#     return request.param
+class dummy_learner:
+    _estimator_type = "regressor"
+    def fit(*args):
+        raise AttributeError("Accessed fit method!")
+    def predict(*args):
+        raise AttributeError("Accessed predict method!")
+    def set_params(*args):
+        raise AttributeError("Accessed set_params method!")
+    def get_params(*args, **kwargs):
+        raise AttributeError("Accessed get_params method!")
+    
 
 @pytest.fixture(scope='module',
                 params=['IV-type', 'partialling out'])
@@ -36,25 +42,21 @@ def adapted_doubleml_fixture(score, dml_procedure, n_rep):
                                    alpha=0.5,
                                    return_type="np.array")
 
-    # lm_m1 = LinearRegression()
-    # lm_l1 = LinearRegression()
-
     np.random.seed(3141)
-
-    # lm_m1.fit(x, d)
-    # ext_predictions['d']['ml_m'] = np.stack([lm_m1.predict(x) for _ in range(n_rep)], axis=1)
-
-    # lm_l1.fit(x, y)
-    # ext_predictions['d']['ml_l'] = np.stack([lm_l1.predict(x) for _ in range(n_rep)], axis=1)
 
     dml_data = DoubleMLData.from_arrays(x=x, y=y, d=d)
 
-    DMLPLR = DoubleMLPLR(obj_dml_data=dml_data,
-                         ml_m=LinearRegression(),
+    kwargs = {'obj_dml_data': dml_data,
+              'score': score,
+              'n_rep': n_rep,
+              'dml_procedure': dml_procedure}
+    
+    if score == 'IV-type':
+        kwargs['ml_g'] = LinearRegression()
+    
+    DMLPLR = DoubleMLPLR(ml_m=LinearRegression(),
                          ml_l=LinearRegression(),
-                         score=score,
-                         n_rep=n_rep,
-                         dml_procedure=dml_procedure)
+                         **kwargs)
     np.random.seed(3141)
 
     DMLPLR.fit(store_predictions=True)
@@ -62,13 +64,14 @@ def adapted_doubleml_fixture(score, dml_procedure, n_rep):
     ext_predictions['d']['ml_m'] = DMLPLR.predictions['ml_m'].squeeze()
     ext_predictions['d']['ml_l'] = DMLPLR.predictions['ml_l'].squeeze()
 
+    if score == 'IV-type':
+        kwargs['ml_g'] = dummy_learner()
+        ext_predictions['d']['ml_g'] = DMLPLR.predictions['ml_g'].squeeze()
 
-    DMLPLR_ext = DoubleMLPLR(obj_dml_data=dml_data,
-                             ml_m=LinearRegression(),
-                             ml_l=LinearRegression(),
-                             score=score,
-                             n_rep=n_rep,
-                             dml_procedure=dml_procedure)
+
+    DMLPLR_ext = DoubleMLPLR(ml_m=dummy_learner(),
+                             ml_l=dummy_learner(),
+                             **kwargs)
 
     np.random.seed(3141)
     DMLPLR_ext.fit(external_predictions=ext_predictions)
