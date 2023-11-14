8ae786758782d5fd7d00fc51d00c811318527c02
Jan Teichert-Kluge <jan.teichertkluge@uni-hamburg.de>
2023-11-14 16:31:38 +0100

    Impl. and Unit Tetsts for DIDCS external preds.

--- doubleml/tests/test_external_predictions_did.py
+++ doubleml/tests/test_external_predictions_did.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 import math
 from sklearn.linear_model import LinearRegression, LassoCV, LogisticRegression
-from doubleml import DoubleMLData, DoubleMLDID
+from doubleml import DoubleMLData, DoubleMLDID, DoubleMLDIDCS
 from doubleml.datasets import make_did_SZ2020
 from doubleml.utils import dummy_regressor, dummy_classifier
 
@@ -49,8 +49,47 @@ def doubleml_did_fixture(did_score, dml_procedure, n_rep):
 
     return res_dict
 
+
+@pytest.fixture(scope="module")
+def doubleml_didcs_fixture(did_score, dml_procedure, n_rep):
+    ext_predictions = {"d": {}}
+
+    np.random.seed(3141)
+
+    dml_data = make_did_SZ2020(n_obs=500, cross_sectional_data=True, return_type="DoubleMLData")
+
+    kwargs = {"obj_dml_data": dml_data, "score": did_score, "n_rep": n_rep, "dml_procedure": dml_procedure}
+
+    DMLDIDCS = DoubleMLDIDCS(ml_g=LinearRegression(), ml_m=LogisticRegression(), **kwargs)
+    np.random.seed(3141)
+
+    DMLDIDCS.fit(store_predictions=True)
+
+    ext_predictions["d"]["ml_g_d0_t0"] = DMLDIDCS.predictions["ml_g_d0_t0"][:, :, 0]
+    ext_predictions["d"]["ml_g_d0_t1"] = DMLDIDCS.predictions["ml_g_d0_t1"][:, :, 0]
+    ext_predictions["d"]["ml_g_d1_t0"] = DMLDIDCS.predictions["ml_g_d1_t0"][:, :, 0]
+    ext_predictions["d"]["ml_g_d1_t1"] = DMLDIDCS.predictions["ml_g_d1_t1"][:, :, 0]
+    ext_predictions["d"]["ml_m"] = DMLDIDCS.predictions["ml_m"][:, :, 0]
+
+    DMLDIDCS_ext = DoubleMLDIDCS(ml_g=dummy_regressor(), ml_m=dummy_classifier(), **kwargs)
+
+    np.random.seed(3141)
+    DMLDIDCS_ext.fit(external_predictions=ext_predictions)
+
+    res_dict = {"coef_normal": DMLDIDCS.coef, "coef_ext": DMLDIDCS_ext.coef}
+
+    return res_dict
+
+
+
 @pytest.mark.ci
 def test_doubleml_did_coef(doubleml_did_fixture):
     assert math.isclose(
         doubleml_did_fixture["coef_normal"], doubleml_did_fixture["coef_ext"], rel_tol=1e-9, abs_tol=1e-3
+    )
+
+@pytest.mark.ci
+def test_doubleml_didcs_coef(doubleml_didcs_fixture):
+    assert math.isclose(
+        doubleml_didcs_fixture["coef_normal"], doubleml_didcs_fixture["coef_ext"], rel_tol=1e-9, abs_tol=1e-3
     )
