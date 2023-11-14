dd244399fab6eda648d8a0ad9ffb4796b75d2824
Jan Teichert-Kluge <jan.teichertkluge@uni-hamburg.de>
2023-11-14 15:49:00 +0100

    Refact. Unit Test for ext. predictions

--- doubleml/tests/test_external_predictions.py
+++ doubleml/tests/test_external_predictions.py
@@ -1,14 +1,18 @@
 import numpy as np
 import pytest
 import math
-from sklearn.linear_model import LinearRegression, LassoCV
-from doubleml import DoubleMLPLR, DoubleMLData
-from doubleml.datasets import make_plr_CCDDHNR2018
-from doubleml.utils import dummy_regressor
+from sklearn.linear_model import LinearRegression, LassoCV, LogisticRegression
+from doubleml import DoubleMLPLR, DoubleMLIRM, DoubleMLData
+from doubleml.datasets import make_plr_CCDDHNR2018, make_irm_data
+from doubleml.utils import dummy_regressor, dummy_classifier
 
 
 @pytest.fixture(scope="module", params=["IV-type", "partialling out"])
-def score(request):
+def plr_score(request):
+    return request.param
+
+@pytest.fixture(scope="module", params=["ATE", "ATTE"])
+def irm_score(request):
     return request.param
 
 
@@ -23,7 +27,7 @@ def n_rep(request):
 
 
 @pytest.fixture(scope="module")
-def adapted_doubleml_fixture(score, dml_procedure, n_rep):
+def doubleml_plr_fixture(plr_score, dml_procedure, n_rep):
     ext_predictions = {"d": {}}
 
     x, y, d = make_plr_CCDDHNR2018(n_obs=500, dim_x=20, alpha=0.5, return_type="np.array")
@@ -32,9 +36,9 @@ def adapted_doubleml_fixture(score, dml_procedure, n_rep):
 
     dml_data = DoubleMLData.from_arrays(x=x, y=y, d=d)
 
-    kwargs = {"obj_dml_data": dml_data, "score": score, "n_rep": n_rep, "dml_procedure": dml_procedure}
+    kwargs = {"obj_dml_data": dml_data, "score": plr_score, "n_rep": n_rep, "dml_procedure": dml_procedure}
 
-    if score == "IV-type":
+    if plr_score == "IV-type":
         kwargs["ml_g"] = LinearRegression()
 
     DMLPLR = DoubleMLPLR(ml_m=LinearRegression(), ml_l=LinearRegression(), **kwargs)
@@ -45,7 +49,7 @@ def adapted_doubleml_fixture(score, dml_procedure, n_rep):
     ext_predictions["d"]["ml_m"] = DMLPLR.predictions["ml_m"][:, :, 0]
     ext_predictions["d"]["ml_l"] = DMLPLR.predictions["ml_l"][:, :, 0]
 
-    if score == "IV-type":
+    if plr_score == "IV-type":
         kwargs["ml_g"] = dummy_regressor()
         ext_predictions["d"]["ml_g"] = DMLPLR.predictions["ml_g"][:, :, 0]
 
@@ -59,8 +63,45 @@ def adapted_doubleml_fixture(score, dml_procedure, n_rep):
     return res_dict
 
 
+@pytest.fixture(scope="module")
+def doubleml_irm_fixture(irm_score, dml_procedure, n_rep):
+    ext_predictions = {"d": {}}
+
+    x, y, d = make_irm_data(n_obs=500, dim_x=20, theta=0.5, return_type="np.array")
+
+    np.random.seed(3141)
+
+    dml_data = DoubleMLData.from_arrays(x=x, y=y, d=d)
+
+    kwargs = {"obj_dml_data": dml_data, "score": irm_score, "n_rep": n_rep, "dml_procedure": dml_procedure}
+
+    DMLIRM = DoubleMLIRM(ml_g=LinearRegression(), ml_m=LogisticRegression(), **kwargs)
+    np.random.seed(3141)
+
+    DMLIRM.fit(store_predictions=True)
+
+    ext_predictions["d"]["ml_g0"] = DMLIRM.predictions["ml_g0"][:, :, 0]
+    ext_predictions["d"]["ml_g1"] = DMLIRM.predictions["ml_g1"][:, :, 0]
+    ext_predictions["d"]["ml_m"] = DMLIRM.predictions["ml_m"][:, :, 0]
+
+    DMLIRM_ext = DoubleMLIRM(ml_g=dummy_regressor(), ml_m=dummy_classifier(), **kwargs)
+
+    np.random.seed(3141)
+    DMLIRM_ext.fit(external_predictions=ext_predictions)
+
+    res_dict = {"coef_normal": DMLIRM.coef, "coef_ext": DMLIRM_ext.coef}
+
+    return res_dict
+
+
 @pytest.mark.ci
-def test_adapted_doubleml_coef(adapted_doubleml_fixture):
+def test_doubleml_plr_coef(doubleml_plr_fixture):
     assert math.isclose(
-        adapted_doubleml_fixture["coef_normal"], adapted_doubleml_fixture["coef_ext"], rel_tol=1e-9, abs_tol=1e-4
+        doubleml_plr_fixture["coef_normal"], doubleml_plr_fixture["coef_ext"], rel_tol=1e-9, abs_tol=1e-4
+    )
+    
+@pytest.mark.ci
+def test_doubleml_irm_coef(doubleml_irm_fixture):
+    assert math.isclose(
+        doubleml_irm_fixture["coef_normal"], doubleml_irm_fixture["coef_ext"], rel_tol=1e-9, abs_tol=1e-4
     )
