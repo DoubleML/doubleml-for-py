b8764d00a7516dbdbaa96c24ac4c11246411b5eb
Sven1704 <sven.klaassen@uni-hamburg.de>
2023-12-22 09:38:37 +0100

    update utils init

--- doubleml/utils/__init__.py
+++ doubleml/utils/__init__.py
@@ -1,11 +1,19 @@
+"""
+The :mod:`doubleml.utils` module includes various utilities.
+"""
+
 from .dummy_learners import DMLDummyRegressor
 from .dummy_learners import DMLDummyClassifier
 
 from .resampling import DoubleMLResampling, DoubleMLClusterResampling
+from .blp import DoubleMLBLP
+from .policytree import DoubleMLPolicyTree
 
 __all__ = [
     "DMLDummyRegressor",
     "DMLDummyClassifier",
     "DoubleMLResampling",
     "DoubleMLClusterResampling",
+    "DoubleMLBLP",
+    "DoubleMLPolicyTree",
 ]
