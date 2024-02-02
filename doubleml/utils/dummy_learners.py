8431daf98e0debbc6ed5d5756216239591504ae7
Sven Klaassen <47529404+SvenKlaassen@users.noreply.github.com>
2024-02-02 11:16:50 +0100

    fix gain statistics and dummy learner docstrings

--- doubleml/utils/dummy_learners.py
+++ doubleml/utils/dummy_learners.py
@@ -6,30 +6,32 @@ class DMLDummyRegressor(BaseEstimator):
     A dummy regressor that raises an AttributeError when attempting to access
     its fit, predict, or set_params methods.
 
-    Attributes
+    Parameters
     ----------
-    _estimator_type : str
-        Type of the estimator, set to "regressor".
 
-    Methods
-    -------
-    fit(*args)
-        Raises AttributeError: "Accessed fit method of DummyRegressor!"
-    predict(*args)
-        Raises AttributeError: "Accessed predict method of DummyRegressor!"
-    set_params(*args)
-        Raises AttributeError: "Accessed set_params method of DummyRegressor!"
     """
 
     _estimator_type = "regressor"
 
     def fit(*args):
+        """
+        Raises AttributeError: "Accessed fit method of DummyRegressor!"
+        """
+
         raise AttributeError("Accessed fit method of DMLDummyRegressor!")
 
     def predict(*args):
+        """
+        Raises AttributeError: "Accessed predict method of DummyRegressor!"
+        """
+
         raise AttributeError("Accessed predict method of DMLDummyRegressor!")
 
     def set_params(*args):
+        """
+        Raises AttributeError: "Accessed set_params method of DummyRegressor!"
+        """
+
         raise AttributeError("Accessed set_params method of DMLDummyRegressor!")
 
 
@@ -38,33 +40,37 @@ class DMLDummyClassifier(BaseEstimator):
     A dummy classifier that raises an AttributeError when attempting to access
     its fit, predict, set_params, or predict_proba methods.
 
-    Attributes
+    Parameters
     ----------
-    _estimator_type : str
-        Type of the estimator, set to "classifier".
 
-    Methods
-    -------
-    fit(*args)
-        Raises AttributeError: "Accessed fit method of DummyClassifier!"
-    predict(*args)
-        Raises AttributeError: "Accessed predict method of DummyClassifier!"
-    set_params(*args)
-        Raises AttributeError: "Accessed set_params method of DummyClassifier!"
-    predict_proba(*args, **kwargs)
-        Raises AttributeError: "Accessed predict_proba method of DummyClassifier!"
     """
 
     _estimator_type = "classifier"
 
     def fit(*args):
+        """
+        Raises AttributeError: "Accessed fit method of DummyClassifier!"
+        """
+
         raise AttributeError("Accessed fit method of DMLDummyClassifier!")
 
     def predict(*args):
+        """
+        Raises AttributeError: "Accessed predict method of DummyClassifier!"
+        """
+
         raise AttributeError("Accessed predict method of DMLDummyClassifier!")
 
     def set_params(*args):
+        """
+        Raises AttributeError: "Accessed set_params method of DummyClassifier!"
+        """
+
         raise AttributeError("Accessed set_params method of DMLDummyClassifier!")
 
     def predict_proba(*args, **kwargs):
+        """
+        Raises AttributeError: "Accessed predict_proba method of DummyClassifier!"
+        """
+
         raise AttributeError("Accessed predict_proba method of DMLDummyClassifier!")
