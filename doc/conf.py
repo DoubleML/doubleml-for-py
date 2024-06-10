2f9fb1fea34a7fb9b8da25a7f64aad78bdfbaa53
Sven Klaassen <47529404+SvenKlaassen@users.noreply.github.com>
2024-06-10 14:59:39 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Bach, P., Chernozhukov, V., Kurz, M. S., and Spindler, M.'
 author = 'Bach, P., Chernozhukov, V., Kurz, M. S., and Spindler, M.'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.dev0'
+release = '0.8.1'
 
 
 # -- General configuration ---------------------------------------------------
