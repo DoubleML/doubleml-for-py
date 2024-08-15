8cf50a14cc3f4012635a86288aae0d25c486a9b8
OliverSchacht <65898638+OliverSchacht@users.noreply.github.com>
2024-08-15 15:19:36 +0200

    fix data gen in test

--- doubleml/rdd/tests/test_rdd_default_values.py
+++ doubleml/rdd/tests/test_rdd_default_values.py
@@ -3,17 +3,22 @@ import numpy as np
 import pandas as pd
 
 import doubleml as dml
+from doubleml.rdd import RDFlex
 from doubleml.rdd.datasets import make_simple_rdd_data
 
 from sklearn.linear_model import Lasso, LogisticRegression
 
 np.random.seed(3141)
 
-data_rdd = make_simple_rdd_data(n_obs=300)
-data_rdd = pd.DataFrame(data_rdd).drop(columns=["oracle_values"])
-dml_data_rdd = dml.DoubleMLData(data_rdd, y_col="Y", d_cols="D", s_col="score")
+n_obs = 300
+data = make_simple_rdd_data(n_obs=n_obs)
+df = pd.DataFrame(
+    np.column_stack((data['Y'], data['D'], data['score'], data['X'])),
+    columns=['y', 'd', 'score'] + ['x' + str(i) for i in range(data['X'].shape[1])]
+)
+dml_data = dml.DoubleMLData(df, y_col='y', d_cols='d', s_col='score')
 
-dml_rdflex = dml.rdd.RDFlex(dml_data_rdd, ml_g=Lasso(), ml_m=LogisticRegression())
+dml_rdflex = RDFlex(dml_data, ml_g=Lasso(), ml_m=LogisticRegression())
 
 
 def _assert_resampling_default_settings(dml_obj):
