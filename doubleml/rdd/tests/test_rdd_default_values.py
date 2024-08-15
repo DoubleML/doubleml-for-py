b3ae1b90bec4b8c024024dab1c497b399b39df2b
OliverSchacht <65898638+OliverSchacht@users.noreply.github.com>
2024-08-15 16:53:06 +0200

    fix: test for initial h_fs

--- doubleml/rdd/tests/test_rdd_default_values.py
+++ doubleml/rdd/tests/test_rdd_default_values.py
@@ -25,7 +25,7 @@ def _assert_resampling_default_settings(dml_obj):
     assert dml_obj.n_folds == 5
     assert dml_obj.n_rep == 1
     assert dml_obj.cutoff == 0
-    assert dml_obj.h_fs is None
+    assert dml_obj.h_fs is not None
     assert dml_obj.fs_kernel == "uniform"
 
 
