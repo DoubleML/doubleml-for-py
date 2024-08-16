dcfce650c8220ad5ef0f6a1b50b934b7846e5952
Sven Klaassen <47529404+SvenKlaassen@users.noreply.github.com>
2024-08-16 08:26:16 +0200

    update simple rdd dgp

--- doubleml/rdd/datasets.py
+++ doubleml/rdd/datasets.py
@@ -1,23 +1,22 @@
 import numpy as np
 
 
-def make_simple_rdd_data(n_obs=5000, **kwargs):
-    dim_x = kwargs.get('dim_x', 5)
+def make_simple_rdd_data(n_obs=5000, fuzzy=True, **kwargs):
+    dim_x = kwargs.get('dim_x', 4)
     score = np.random.uniform(size=n_obs, low=-1, high=1)
     D = (score >= 0)
-    # Make it fuzzy
-    D[:300] = np.random.binomial(n=1, size=300, p=0.5)
+    if fuzzy:
+        D = np.random.binomial(n=1, p=0.3 + 0.2 * D, size=n_obs)
     D = D.astype("int")
 
     # independent covariates
     X = np.random.uniform(size=(n_obs, dim_x), low=-1, high=1)
-    g0 = X[:, 0] + score*X[:, 1]**2 + 3*np.sin(X[:, 2])
+    g0 = X[:, 0] + 5*X[:, 1]**2 + 3*np.sin(X[:, 2])
     g1 = g0
 
-    epsilon = np.random.normal(size=n_obs)
-    # potential outcomes
-    Y0 = - score**2 + g0 + epsilon
-    Y1 = 1 + score**2 + g1 + epsilon
+    # potential outcomes with independent errors
+    Y0 = - score**2 + g0 + np.random.normal(size=n_obs)
+    Y1 = 1 + score**2 + g1 + np.random.normal(size=n_obs)
 
     Y = Y0 * (1 - D) + Y1 * D
 
