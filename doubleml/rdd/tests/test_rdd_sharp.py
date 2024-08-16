5eb2f810f30e5a44576f669169b5807caf3d1f14
Sven Klaassen <47529404+SvenKlaassen@users.noreply.github.com>
2024-08-16 16:17:30 +0200

    add se to test_rdd_sharp.py

--- doubleml/rdd/tests/test_rdd_sharp.py
+++ doubleml/rdd/tests/test_rdd_sharp.py
@@ -26,29 +26,54 @@ def cutoff(request):
     return request.param
 
 
+@pytest.fixture(scope='module',
+                params=[1, 2])
+def p(request):
+    return request.param
+
+
 @pytest.fixture(scope='module')
-def rdd_zero_predictions_fixture(cutoff):
+def rdd_zero_predictions_fixture(cutoff, p):
+    kwargs = {
+        'p': p
+    }
+
+    # set the treatment indicator correctly based on the cutoff
     df['d'] = (data['score'] >= cutoff).astype(bool)
     dml_data = DoubleMLData(df, y_col='y', d_cols='d', s_col='score')
+
     dml_rdflex = RDFlex(
         dml_data,
         ml_g=ml_g_dummy,
         ml_m=ml_m_dummy,
-        cutoff=cutoff)
+        cutoff=cutoff,
+        **kwargs)
     dml_rdflex.fit(n_iterations=1)
 
-    rdrobust_model = rdrobust(y=df['y'], x=df['score'], c=cutoff)
+    rdrobust_model = rdrobust(y=df['y'], x=df['score'], c=cutoff, **kwargs)
 
     res_dict = {
         'dml_rdflex': dml_rdflex,
-        'rdrobust_model': rdrobust_model
+        'dml_coef': dml_rdflex.coef,
+        'dml_se': dml_rdflex.se,
+        'rdrobust_model': rdrobust_model,
+        'rdrobust_coef': rdrobust_model.coef.values.flatten(),
+        'rdrobust_se': rdrobust_model.se.values.flatten()
     }
     return res_dict
 
 
 @pytest.mark.ci
 def test_rdd_coef(rdd_zero_predictions_fixture):
-    dml_coef = rdd_zero_predictions_fixture['dml_rdflex'].coef
-    rdrobust_coef = rdd_zero_predictions_fixture['rdrobust_model'].coef.values.flatten()
+    dml_coef = rdd_zero_predictions_fixture['dml_coef']
+    rdrobust_coef = rdd_zero_predictions_fixture['rdrobust_coef']
 
     assert np.allclose(dml_coef, rdrobust_coef, rtol=1e-9, atol=1e-4)
+
+
+@pytest.mark.ci
+def test_rdd_se(rdd_zero_predictions_fixture):
+    dml_se = rdd_zero_predictions_fixture['dml_se']
+    rdrobust_se = rdd_zero_predictions_fixture['rdrobust_se']
+
+    assert np.allclose(dml_se, rdrobust_se, rtol=1e-9, atol=1e-4)
