80d68e608847d0e199daef51070f66380d5d3c1c
OliverSchacht <65898638+OliverSchacht@users.noreply.github.com>
2024-09-03 15:46:02 +0200

    avoid warning

--- doubleml/rdd/tests/test_rdd_default_values.py
+++ doubleml/rdd/tests/test_rdd_default_values.py
@@ -11,7 +11,7 @@ from sklearn.linear_model import Lasso, LogisticRegression
 np.random.seed(3141)
 
 n_obs = 300
-data = make_simple_rdd_data(n_obs=n_obs)
+data = make_simple_rdd_data(n_obs=n_obs, fuzzy=False)
 df = pd.DataFrame(
     np.column_stack((data['Y'], data['D'], data['score'], data['X'])),
     columns=['y', 'd', 'score'] + ['x' + str(i) for i in range(data['X'].shape[1])]
