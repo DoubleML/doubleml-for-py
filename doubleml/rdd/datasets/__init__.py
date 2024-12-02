61cf5cb312331a58bacc3e369a3d461173097f2a
Sven Klaassen <47529404+SvenKlaassen@users.noreply.github.com>
2024-12-02 11:30:48 +0100

    add rdd data generators to init

--- doubleml/rdd/datasets/__init__.py
+++ doubleml/rdd/datasets/__init__.py
@@ -1,2 +1,11 @@
+"""
+The :mod:`doubleml.rdd.datasets` module implements data generating processes for regression discontinuity designs.
+"""
+
 from .simple_dgp import make_simple_rdd_data
 from .area_yield_dgp import dgp_area_yield
+
+__all__ = [
+    "make_simple_rdd_data",
+    "dgp_area_yield",
+]
