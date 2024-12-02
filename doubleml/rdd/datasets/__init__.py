6fff8c50da4e39d475da11d0b25b85f19554986c
Oliver Schacht <oliver.schacht@uni-hamburg.de>
2024-12-02 15:55:47 +0100

    remove area yield from PR

--- doubleml/rdd/datasets/__init__.py
+++ doubleml/rdd/datasets/__init__.py
@@ -3,9 +3,7 @@ The :mod:`doubleml.rdd.datasets` module implements data generating processes for
 """
 
 from .simple_dgp import make_simple_rdd_data
-from .area_yield_dgp import dgp_area_yield
 
 __all__ = [
     "make_simple_rdd_data",
-    "dgp_area_yield",
 ]
