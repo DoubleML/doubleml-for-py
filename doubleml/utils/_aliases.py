ade708bf75899f07b04b613dba3b29ac7613dd0d
SvenKlaassen <sven.klaassen@uni-hamburg.de>
2025-01-29 15:09:01 +0100

    remove not used warnings from aliases

--- doubleml/utils/_aliases.py
+++ doubleml/utils/_aliases.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from ..double_ml_data import DoubleMLClusterData, DoubleMLData
 
-_array_alias = ["array", "np.ndarray", "np.array", np.ndarray]
-_data_frame_alias = ["DataFrame", "pd.DataFrame", pd.DataFrame]
-_dml_data_alias = ["DoubleMLData", DoubleMLData]
-_dml_cluster_data_alias = ["DoubleMLClusterData", DoubleMLClusterData]
+_array_alias = ["array", "np.ndarray", "np.array", np.ndarray]  # noqa
+_data_frame_alias = ["DataFrame", "pd.DataFrame", pd.DataFrame]  # noqa
+_dml_data_alias = ["DoubleMLData", DoubleMLData]  # noqa
+_dml_cluster_data_alias = ["DoubleMLClusterData", DoubleMLClusterData]  # noqa
