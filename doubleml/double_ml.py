6f05b1126c49488fa8c4299db90af8e2147179ac
SvenKlaassen <sven.klaassen@uni-hamburg.de>
2025-02-05 09:37:33 +0100

    remove unnecessary assignment

--- doubleml/double_ml.py
+++ doubleml/double_ml.py
@@ -1432,7 +1432,6 @@ class DoubleML(ABC):
         if self._sensitivity_implemented:
             for i_treat in range(self._dml_data.n_treat):
                 nu2 = self.sensitivity_elements["nu2"][:, :, i_treat]
-                psi_nu2 = self.sensitivity_elements["psi_nu2"][:, :, i_treat]
                 riesz_rep = self.sensitivity_elements["riesz_rep"][:, :, i_treat]
 
                 if np.any(nu2 <= 0):
@@ -1442,8 +1441,8 @@ class DoubleML(ABC):
                         "Re-estimation based on riesz representer (non-orthogonal)."
                     )
                     warnings.warn(msg, UserWarning)
-                    nu2 = np.mean(np.power(riesz_rep, 2), axis=0, keepdims=True)
                     psi_nu2 = np.power(riesz_rep, 2)
+                    nu2 = np.mean(psi_nu2, axis=0, keepdims=True)
 
                     self.sensitivity_elements["nu2"][:, :, i_treat] = nu2
                     self.sensitivity_elements["psi_nu2"][:, :, i_treat] = psi_nu2
