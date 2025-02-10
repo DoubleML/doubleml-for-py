0ae0aedcd943f37e82348d34e3ab59cdb33728ae
SvenKlaassen <sven.klaassen@uni-hamburg.de>
2025-02-10 08:26:03 +0100

    use np.clip

--- doubleml/utils/_propensity_score.py
+++ doubleml/utils/_propensity_score.py
@@ -4,7 +4,7 @@ import numpy as np
 def _trimm(preds, trimming_rule, trimming_threshold):
     """Trim predictions based on the specified method and threshold."""
     if trimming_rule == "truncate":
-        adjusted_predictions = np.clip(preds, trimming_threshold, 1 - trimming_threshold)
+        adjusted_predictions = np.clip(a=preds, a_min=trimming_threshold, a_max=1 - trimming_threshold)
     return adjusted_predictions
 
 
