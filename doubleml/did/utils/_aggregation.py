fix time aggregation scaling

doubleml/did/utils/_aggregation.py
@@ -148,7 +148,7 @@ def _compute_did_time_aggregation_weights(gt_index, g_values, t_values, d_values
         time_gt_indicies = [(i, j, k) for i, j, k in zip(*selected_gt_indicies) if k == t_eval_idx]
 
         group_weights = np.zeros(len(g_values))
-        selected_unique_g_indices = np.unique(selected_gt_indicies[0])
+        selected_unique_g_indices = np.unique(time_gt_indicies[0])
         for g_idx in selected_unique_g_indices:
             group_weights[g_idx] = (d_values == g_values[g_idx]).mean()  # (requires balanced panel)
         # normalize weights