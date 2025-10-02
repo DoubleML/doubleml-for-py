b00b7ae90eb0cca5f7244481032d13365a66a42f
Jan Teichert-Kluge <janteiklu@gmail.com>
2025-10-02 10:15:50 +0200

    fix n_obs in APOs

--- doubleml/irm/apos.py
+++ doubleml/irm/apos.py
@@ -88,6 +88,7 @@ class DoubleMLAPOS(SampleSplittingMixin):
 
         # perform sample splitting
         self._smpls = None
+        self._n_obs_sample_splitting = self._dml_data.n_obs
         if draw_sample_splitting:
             self.draw_sample_splitting()
 
