270ac04f0291410d02477b54f948e779b090f563
Jan Teichert-Kluge <janteiklu@gmail.com>
2025-11-24 14:19:41 +0100

    change arg `force_all_finite` to `ensure_all_finite` for logistic model (DoubleMLLPLR)

--- doubleml/plm/lplr.py
+++ doubleml/plm/lplr.py
@@ -423,8 +423,8 @@ class DoubleMLLPLR(NonLinearScoreMixin, DoubleML):
     def _nuisance_tuning(
         self, smpls, param_grids, scoring_methods, n_folds_tune, n_jobs_cv, search_mode, n_iter_randomized_search
     ):
-        x, y = check_X_y(self._dml_data.x, self._dml_data.y, force_all_finite=False)
-        x, d = check_X_y(x, self._dml_data.d, force_all_finite=False)
+        x, y = check_X_y(self._dml_data.x, self._dml_data.y, ensure_all_finite=False)
+        x, d = check_X_y(x, self._dml_data.d, ensure_all_finite=False)
         x_d_concat = np.hstack((d.reshape(-1, 1), x))
 
         if scoring_methods is None:
