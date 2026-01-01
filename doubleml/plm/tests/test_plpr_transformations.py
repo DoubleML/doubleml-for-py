32b44fefe22b995e2ba4e415c4e7e81d41bb0636
Julian Diefenbacher <93731825+JulianDiefenbacher@users.noreply.github.com>
2026-01-02 00:43:29 +0100

    improve plpr transformation test structure

--- doubleml/plm/tests/test_plpr_transformations.py
+++ doubleml/plm/tests/test_plpr_transformations.py
@@ -14,22 +14,7 @@ learner = LinearRegression()
 ml_l = clone(learner)
 ml_m = clone(learner)
 
-np.random.seed(3141)
-plpr_data = make_plpr_CP2025(dim_x=dim_x)
-obj_dml_data = dml.DoubleMLPanelData(
-    plpr_data,
-    y_col="y",
-    d_cols="d",
-    t_col="time",
-    id_col="id",
-    static_panel=True,
-)
-dml_plpr_cre_general = dml.DoubleMLPLPR(obj_dml_data, ml_l, ml_m, approach="cre_general")
-dml_plpr_cre_normal = dml.DoubleMLPLPR(obj_dml_data, ml_l, ml_m, approach="cre_normal")
-dml_plpr_fd_exact = dml.DoubleMLPLPR(obj_dml_data, ml_l, ml_m, approach="fd_exact")
-dml_plpr_wg_approx = dml.DoubleMLPLPR(obj_dml_data, ml_l, ml_m, approach="wg_approx")
-
-df = pd.DataFrame(
+df_int = pd.DataFrame(
     {
         "id": [1, 1, 2, 2],
         "time": [1, 2, 1, 2],
@@ -39,14 +24,12 @@ df = pd.DataFrame(
         "x2": [1, 2, 0, 2],
     }
 )
-df_dml_data = dml.DoubleMLPanelData(
-    df,
-    y_col="y",
-    d_cols="d",
-    t_col="time",
-    id_col="id",
-    static_panel=True,
-)
+df_float = df_int.copy()
+df_float["time"] = df_float["time"].astype(float)
+
+df_datetime = df_int.copy()
+df_datetime["time"] = pd.to_datetime([f"2020-{int(t):02d}-01" for t in df_datetime["time"]])
+
 cre_array = np.array(
     [
         [1, 1, 1, 1, 1, 1.5],
@@ -69,7 +52,7 @@ wg_array = np.array(
         [7.25, 3, 1.25, 2.25],
     ]
 )
-df_unbalanced = pd.DataFrame(
+df_unbalance_int = pd.DataFrame(
     {
         "id": [1, 1, 2, 2, 3],
         "time": [1, 2, 1, 2, 2],
@@ -79,14 +62,17 @@ df_unbalanced = pd.DataFrame(
         "x2": [1, 2, 0, 2, 1],
     }
 )
-df_unbalanced_dml_data = dml.DoubleMLPanelData(
-    df_unbalanced,
-    y_col="y",
-    d_cols="d",
-    t_col="time",
-    id_col="id",
-    static_panel=True,
-)
+
+df_unbalance_float = df_unbalance_int.copy()
+df_unbalance_float["time"] = df_unbalance_float["time"].astype(float)
+
+df_unbalance_datetime = df_unbalance_int.copy()
+df_unbalance_datetime["time"] = pd.to_datetime([f"2020-{int(t):02d}-01" for t in df_unbalance_datetime["time"]])
+
+
+@pytest.fixture(scope="module", params=["cre_general", "cre_normal", "fd_exact", "wg_approx"])
+def approach(request):
+    return request.param
 
 
 @pytest.fixture(scope="module", params=["cre_general", "cre_normal"])
@@ -94,145 +80,130 @@ def cre_approach(request):
     return request.param
 
 
-@pytest.mark.ci
-def test_plpr_approach_x_dim():
-    assert len(dml_plpr_cre_general._dml_data.x_cols) == dim_x * 2
-    assert len(dml_plpr_cre_normal._dml_data.x_cols) == dim_x * 2
-    assert len(dml_plpr_fd_exact._dml_data.x_cols) == dim_x * 2
-    assert len(dml_plpr_wg_approx._dml_data.x_cols) == dim_x
+@pytest.fixture(scope="module", params=["int", "float", "datetime"])
+def time_type(request):
+    return request.param
+
+
+@pytest.fixture(scope="module", params=[df_int, df_float, df_datetime])
+def data_time_type(request):
+    return request.param
+
+
+@pytest.fixture(scope="module", params=[df_unbalance_int, df_unbalance_float, df_unbalance_datetime])
+def data_unbalance_time_type(request):
+    return request.param
 
 
 @pytest.mark.ci
-def test_plpr_approach_d_mean():
-    assert dml_plpr_cre_general.d_mean is not None
-    assert dml_plpr_cre_normal.d_mean is not None
-    assert dml_plpr_fd_exact.d_mean is None
-    assert dml_plpr_wg_approx.d_mean is None
+def test_plpr_approach_x_dim(approach, time_type):
+    np.random.seed(3141)
+    plpr_data = make_plpr_CP2025(dim_x=dim_x, time_type=time_type)
+    obj_dml_data = dml.DoubleMLPanelData(
+        plpr_data,
+        y_col="y",
+        d_cols="d",
+        t_col="time",
+        id_col="id",
+        static_panel=True,
+    )
+    dml_plpr = dml.DoubleMLPLPR(obj_dml_data, ml_l, ml_m, approach=approach)
+    if approach == "wg_approx":
+        assert len(dml_plpr._dml_data.x_cols) == dim_x
+    else:
+        assert len(dml_plpr._dml_data.x_cols) == dim_x * 2
 
 
 @pytest.mark.ci
-def test_plpr_d_mean_calc(cre_approach):
-    dml_obj = dml.DoubleMLPLPR(df_dml_data, ml_l, ml_m, approach=cre_approach, n_folds=2)
+def test_plpr_approach_d_mean(approach, time_type):
+    np.random.seed(3141)
+    plpr_data = make_plpr_CP2025(dim_x=dim_x, time_type=time_type)
+    obj_dml_data = dml.DoubleMLPanelData(
+        plpr_data,
+        y_col="y",
+        d_cols="d",
+        t_col="time",
+        id_col="id",
+        static_panel=True,
+    )
+    dml_plpr = dml.DoubleMLPLPR(obj_dml_data, ml_l, ml_m, approach=approach)
+    if approach in ["cre_general", "cre_normal"]:
+        assert dml_plpr.d_mean is not None
+    else:
+        assert dml_plpr.d_mean is None
+
+
+@pytest.mark.ci
+def test_plpr_d_mean_calc(cre_approach, data_time_type):
+    obj_dml_data = dml.DoubleMLPanelData(
+        data_time_type,
+        y_col="y",
+        d_cols="d",
+        t_col="time",
+        id_col="id",
+        static_panel=True,
+    )
+    dml_obj = dml.DoubleMLPLPR(obj_dml_data, ml_l, ml_m, approach=cre_approach, n_folds=2)
     assert np.array_equal(dml_obj.d_mean, np.array([1.5, 1.5, 3.5, 3.5]).reshape(-1, 1))
 
 
 @pytest.mark.ci
-def test_plpr_cre_transformation(cre_approach):
-    dml_obj = dml.DoubleMLPLPR(df_dml_data, ml_l, ml_m, approach=cre_approach, n_folds=2)
-    assert dml_obj.transform_cols["y_col"] == "y"
-    assert dml_obj.transform_cols["d_cols"] == ["d"]
-    assert dml_obj.transform_cols["x_cols"] == ["x1", "x2", "x1_mean", "x2_mean"]
-    assert np.array_equal(dml_obj.data_transform.y, cre_array[:, 0])
-    assert np.array_equal(dml_obj.data_transform.d, cre_array[:, 1])
-    assert np.array_equal(dml_obj.data_transform.x, cre_array[:, 2:])
-
-
-@pytest.mark.ci
-def test_plpr_fd_exact_transformation():
-    dml_obj = dml.DoubleMLPLPR(df_dml_data, ml_l, ml_m, approach="fd_exact", n_folds=2)
-    assert dml_obj.transform_cols["y_col"] == "y_diff"
-    assert dml_obj.transform_cols["d_cols"] == ["d_diff"]
-    assert dml_obj.transform_cols["x_cols"] == ["x1", "x2", "x1_lag", "x2_lag"]
-    assert np.array_equal(dml_obj.data_transform.y, fd_array[:, 0])
-    assert np.array_equal(dml_obj.data_transform.d, fd_array[:, 1])
-    assert np.array_equal(dml_obj.data_transform.x, fd_array[:, 2:])
-
-
-@pytest.mark.ci
-def test_plpr_wg_approx_transformation():
-    dml_obj = dml.DoubleMLPLPR(df_dml_data, ml_l, ml_m, approach="wg_approx", n_folds=2)
-    assert dml_obj.transform_cols["y_col"] == "y_demean"
-    assert dml_obj.transform_cols["d_cols"] == ["d_demean"]
-    assert dml_obj.transform_cols["x_cols"] == ["x1_demean", "x2_demean"]
-    assert np.array_equal(dml_obj.data_transform.y, wg_array[:, 0])
-    assert np.array_equal(dml_obj.data_transform.d, wg_array[:, 1])
-    assert np.array_equal(dml_obj.data_transform.x, wg_array[:, 2:])
-
-
-@pytest.mark.ci
-def test_plpr_fd_exact_unbalanced():
+def test_plpr_fd_exact_unbalanced(data_unbalance_time_type):
     msg_warn = r"The panel data contains 1 missing \(id, time\) combinations. "
+    obj_dml_data = dml.DoubleMLPanelData(
+        data_unbalance_time_type,
+        y_col="y",
+        d_cols="d",
+        t_col="time",
+        id_col="id",
+        static_panel=True,
+    )
+
     with pytest.warns(UserWarning, match=msg_warn):
-        _ = dml.DoubleMLPLPR(df_unbalanced_dml_data, ml_l, ml_m, approach="fd_exact", n_folds=2)
+        _ = dml.DoubleMLPLPR(obj_dml_data, ml_l, ml_m, approach="fd_exact", n_folds=2)
 
 
 @pytest.mark.ci
-def test_plpr_time_float_cre_transformation(cre_approach):
-    df_float = df.copy()
-    df_float["time"] = df_float["time"].astype(float)
-    df_dml = dml.DoubleMLPanelData(
-        df_float,
+def test_plpr_time_cre_transformation(cre_approach, data_time_type):
+    obj_dml_data = dml.DoubleMLPanelData(
+        data_time_type,
         y_col="y",
         d_cols="d",
         t_col="time",
         id_col="id",
         static_panel=True,
     )
-    dml_obj = dml.DoubleMLPLPR(df_dml, ml_l, ml_m, approach=cre_approach, n_folds=2)
-    assert np.array_equal(dml_obj.data_transform.y, cre_array[:, 0])
-    assert np.array_equal(dml_obj.data_transform.d, cre_array[:, 1])
-    assert np.array_equal(dml_obj.data_transform.x, cre_array[:, 2:])
+    dml_cre = dml.DoubleMLPLPR(obj_dml_data, ml_l, ml_m, approach=cre_approach, n_folds=2)
+    assert dml_cre.transform_cols["y_col"] == "y"
+    assert dml_cre.transform_cols["d_cols"] == ["d"]
+    assert dml_cre.transform_cols["x_cols"] == ["x1", "x2", "x1_mean", "x2_mean"]
+    assert np.array_equal(dml_cre.data_transform.y, cre_array[:, 0])
+    assert np.array_equal(dml_cre.data_transform.d, cre_array[:, 1])
+    assert np.array_equal(dml_cre.data_transform.x, cre_array[:, 2:])
 
 
 @pytest.mark.ci
-def test_plpr_time_float_fd_wg_transformation():
-    df_float = df.copy()
-    df_float["time"] = df_float["time"].astype(float)
-    df_dml = dml.DoubleMLPanelData(
-        df_float,
+def test_plpr_time_fd_wg_transformation(data_time_type):
+    obj_dml_data = dml.DoubleMLPanelData(
+        data_time_type,
         y_col="y",
         d_cols="d",
         t_col="time",
         id_col="id",
         static_panel=True,
     )
-    dml_fd = dml.DoubleMLPLPR(df_dml, ml_l, ml_m, approach="fd_exact", n_folds=2)
+    dml_fd = dml.DoubleMLPLPR(obj_dml_data, ml_l, ml_m, approach="fd_exact", n_folds=2)
+    assert dml_fd.transform_cols["y_col"] == "y_diff"
+    assert dml_fd.transform_cols["d_cols"] == ["d_diff"]
+    assert dml_fd.transform_cols["x_cols"] == ["x1", "x2", "x1_lag", "x2_lag"]
     assert np.array_equal(dml_fd.data_transform.y, fd_array[:, 0])
     assert np.array_equal(dml_fd.data_transform.d, fd_array[:, 1])
     assert np.array_equal(dml_fd.data_transform.x, fd_array[:, 2:])
 
-    dml_wg = dml.DoubleMLPLPR(df_dml, ml_l, ml_m, approach="wg_approx", n_folds=2)
-    assert np.array_equal(dml_wg.data_transform.y, wg_array[:, 0])
-    assert np.array_equal(dml_wg.data_transform.d, wg_array[:, 1])
-    assert np.array_equal(dml_wg.data_transform.x, wg_array[:, 2:])
-
-
-@pytest.mark.ci
-def test_plpr_time_datetime_cre_transformation(cre_approach):
-    df_dt = df.copy()
-    df_dt["time"] = pd.to_datetime(["2020-01-01", "2020-02-01", "2020-01-01", "2020-02-01"])
-    df_dml = dml.DoubleMLPanelData(
-        df_dt,
-        y_col="y",
-        d_cols="d",
-        t_col="time",
-        id_col="id",
-        static_panel=True,
-    )
-    dml_obj = dml.DoubleMLPLPR(df_dml, ml_l, ml_m, approach=cre_approach, n_folds=2)
-    assert np.array_equal(dml_obj.data_transform.y, cre_array[:, 0])
-    assert np.array_equal(dml_obj.data_transform.d, cre_array[:, 1])
-    assert np.array_equal(dml_obj.data_transform.x, cre_array[:, 2:])
-
-
-@pytest.mark.ci
-def test_plpr_time_datetime_fd_wg_transformation():
-    df_dt = df.copy()
-    df_dt["time"] = pd.to_datetime(["2020-01-01", "2020-02-01", "2020-01-01", "2020-02-01"])
-    df_dml = dml.DoubleMLPanelData(
-        df_dt,
-        y_col="y",
-        d_cols="d",
-        t_col="time",
-        id_col="id",
-        static_panel=True,
-    )
-    dml_fd = dml.DoubleMLPLPR(df_dml, ml_l, ml_m, approach="fd_exact", n_folds=2)
-    assert np.array_equal(dml_fd.data_transform.y, fd_array[:, 0])
-    assert np.array_equal(dml_fd.data_transform.d, fd_array[:, 1])
-    assert np.array_equal(dml_fd.data_transform.x, fd_array[:, 2:])
-
-    dml_wg = dml.DoubleMLPLPR(df_dml, ml_l, ml_m, approach="wg_approx", n_folds=2)
+    dml_wg = dml.DoubleMLPLPR(obj_dml_data, ml_l, ml_m, approach="wg_approx", n_folds=2)
+    assert dml_wg.transform_cols["y_col"] == "y_demean"
+    assert dml_wg.transform_cols["d_cols"] == ["d_demean"]
+    assert dml_wg.transform_cols["x_cols"] == ["x1_demean", "x2_demean"]
     assert np.array_equal(dml_wg.data_transform.y, wg_array[:, 0])
     assert np.array_equal(dml_wg.data_transform.d, wg_array[:, 1])
     assert np.array_equal(dml_wg.data_transform.x, wg_array[:, 2:])
