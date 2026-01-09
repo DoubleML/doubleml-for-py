3d523c03f322b3ab913a22a7adb38c33c8d08525
JulianDiefenbacher <julian.diefenbacher@gmx.de>
2026-01-09 23:47:49 +0100

    fix notation in mathematical expression for individual heterogeneity in DoubleMLPLPR

--- doubleml/plm/plpr.py
+++ doubleml/plm/plpr.py
@@ -88,13 +88,13 @@ class DoubleMLPLPR(LinearScoreMixin, DoubleML):
 
     .. math::
 
-        Y_{it} &= D_{it} \\theta_0 + g_0(X_{it}) + \\alpha_i + U_{it}, & &\\mathbb{E}(U_{it} | D_{it},X_{it},\\alpha_i) = 0,
+        Y_{it} &= D_{it} \\theta_0 + g_0(X_{it}) + \\alpha_i^* + U_{it}, & &\\mathbb{E}(U_{it} | D_{it},X_{it},\\alpha_i) = 0,
 
         D_{it} &= m_0(X_{it}) + \\gamma_i + V_{it}, & &\\mathbb{E}(V_{it} | X_{it},\\gamma_i) = 0,
 
     where :math:`Y_{it}` is the outcome variable and :math:`D_{it}` is the policy variable of interest.
     The high-dimensional vector :math:`X_{it} = (X_{it,1}, \\ldots, X_{it,p})` consists of other confounding covariates,
-    :math:`\\alpha_i` and :math:`\\gamma_i` are the unobserved individual heterogeneity correlated with the included
+    :math:`\\alpha_i^*` and :math:`\\gamma_i` are the unobserved individual heterogeneity correlated with the included
     covariates, and :math:`U_{it}` and :math:`V_{it}` are stochastic errors.
     """
 
