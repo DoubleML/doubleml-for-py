96e32c9517812a8b8875dd74956704d7998f6510
SvenKlaassen <sven.klaassen@uni-hamburg.de>
2026-08-10 08:36:48 +0200

    fix: ensure real values are used in sqrtm calculations and adjust quantile axis in bootstrap samples

--- doubleml/utils/tests/_utils_blp_manual.py
+++ doubleml/utils/tests/_utils_blp_manual.py
@@ -22,9 +22,10 @@ def blp_confint(blp_model, basis, joint=False, level=0.95, n_rep_boot=500):
     if joint:
         # calculate the maximum t-statistic with bootstrap
         normal_samples = np.random.normal(size=[basis.shape[1], n_rep_boot])
-        bootstrap_samples = np.multiply(basis.dot(np.dot(sqrtm(blp_omega), normal_samples)).T, (1.0 / blp_se))
+        # shape (n_rep_boot, n_obs_basis); max over the basis points, quantile over the bootstrap draws
+        bootstrap_samples = np.multiply(basis.dot(np.dot(np.real(sqrtm(blp_omega)), normal_samples)).T, (1.0 / blp_se))
 
-        max_t_stat = np.quantile(np.max(np.abs(bootstrap_samples), axis=0), q=level)
+        max_t_stat = np.quantile(np.max(np.abs(bootstrap_samples), axis=1), q=level)
 
         # Lower simultaneous CI
         g_hat_lower = g_hat - max_t_stat * blp_se
